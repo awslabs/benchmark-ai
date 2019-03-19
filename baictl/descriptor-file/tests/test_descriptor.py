de40271ce83f2955f72d4c6a37002ad3a72fa66e
Edison Gustavo Muenz <edisongustavo@gmail.com>
2019-03-19 15:57:47 +0100

    Add tests to descriptor

---
+++ baictl/descriptor-file/tests/test_descriptor.py
@@ -0,0 +1,12 @@
+import pytest
+
+from descriptor_reader import Descriptor
+
+
+@pytest.mark.parametrize('filename', [
+    'missing_keys_descriptor.toml',
+    'missing_section_descriptor.toml'
+])
+def test_wrong_descriptor(datadir, filename):
+    with pytest.raises(KeyError):
+        Descriptor.from_toml_file(str(datadir/filename))
