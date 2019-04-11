a0af9f87a73ef97c0c9314fbb7617509929795ec
Edison Gustavo Muenz <edisongustavo@gmail.com>
2019-04-11 17:53:02 +0200

    Specify the availability zone for jobs explicitly

--- baictl/descriptor-file/tests/test_bai_config.py
+++ baictl/descriptor-file/tests/test_bai_config.py
@@ -35,21 +35,21 @@ def descriptor():
     """)))
 
 
-def test_add_container_cmd(descriptor):
+def test_add_container_cmd(descriptor, bai_environment_info):
     descriptor.benchmark_code = 'cmd'
     descriptor.ml_args = 'arg1 arg2'
 
-    baiconfig = create_bai_config(descriptor)
+    baiconfig = create_bai_config(descriptor, environment_info=bai_environment_info)
     container = baiconfig.root.find_container('benchmark')
     assert container.command == ['cmd', 'arg1', 'arg2']
     assert 'args' not in container
 
 
-def test_add_container_no_cmd(descriptor):
+def test_add_container_no_cmd(descriptor, bai_environment_info):
     descriptor.benchmark_code = ''
     descriptor.ml_args = 'arg1 arg2=abc'
 
-    baiconfig = create_bai_config(descriptor)
+    baiconfig = create_bai_config(descriptor, environment_info=bai_environment_info)
     container = baiconfig.root.find_container('benchmark')
     assert 'command' not in container
     assert container.args == ['arg1', 'arg2=abc']
--- baictl/descriptor-file/tests/test_reader_regressions.py
+++ baictl/descriptor-file/tests/test_reader_regressions.py
@@ -13,11 +13,15 @@ from transpiler.bai_knowledge import create_bai_config
     "training.toml",
     "horovod.toml",
 ])
-def test_regressions(filename, shared_datadir, file_regression: FileRegressionFixture):
+def test_regressions(filename, shared_datadir, file_regression: FileRegressionFixture, bai_environment_info):
     random_object = random.Random()
     random_object.seed(1)
 
     descriptor = Descriptor.from_toml_file(str(shared_datadir / filename))
-    bai_config = create_bai_config(descriptor, extra_bai_config_args=dict(random_object=random_object))
+    bai_config = create_bai_config(
+        descriptor,
+        environment_info=bai_environment_info,
+        extra_bai_config_args=dict(random_object=random_object)
+    )
     basename = os.path.splitext(filename)[0] + "-k8s-object"
     file_regression.check(bai_config.dump_yaml_string(), basename=basename, extension=".yaml")
