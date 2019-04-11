984771b6b011a021258b51c717c2624f3f6f1995
Edison Gustavo Muenz <edisongustavo@gmail.com>
2019-04-11 17:48:16 +0200

    Specify the K8S version to the "eks" terraform module

--- baictl/drivers/aws/cluster/eks.tf
+++ baictl/drivers/aws/cluster/eks.tf
@@ -175,6 +175,7 @@ module "eks" {
   source                               = "terraform-aws-modules/eks/aws"
   cluster_name                         = "${var.cluster_name}"
   subnets                              = ["${module.vpc.private_subnets}"]
+  cluster_version                      = "${var.k8s_version}"
   tags                                 = "${local.tags}"
   vpc_id                               = "${module.vpc.vpc_id}"
   worker_groups                        = "${local.worker_groups}"
