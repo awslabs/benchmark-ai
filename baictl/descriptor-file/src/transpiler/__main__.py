b1363067f558afd7d5fdd76a6cfa366b8bf937ac
Jose Luis Contreras <jlcont@amazon.com>
2019-04-16 13:08:45 +0200

    [baictl] Small refactor in transpiler to add create_yaml_spec function

--- baictl/descriptor-file/src/transpiler/__main__.py
+++ baictl/descriptor-file/src/transpiler/__main__.py
@@ -1,22 +1,21 @@
-import configparser
 import os
+import toml
 
 from transpiler.args import get_args
 from transpiler.descriptor import Descriptor, DescriptorSettings
 from transpiler.bai_knowledge import create_bai_config, EnvironmentInfo
+from typing import Dict
 
 
 def main(argv=None):
     args = get_args(argv)
 
-    descriptor_config = DescriptorSettings(**args)
-    descriptor = Descriptor.from_toml_file(args.descriptor, descriptor_config)
-
+    descriptor_data = toml.load(args.descriptor)
     environment_info = EnvironmentInfo(
         availability_zones=args.availability_zones
     )
-    bai_config = create_bai_config(descriptor, args, environment_info=environment_info)
-    yaml_string = bai_config.dump_yaml_string()
+
+    yaml_string = create_yaml_spec(descriptor_data, args, environment_info)
 
     if args.filename:
         current_dir = os.path.dirname(os.path.abspath(__file__))
@@ -26,14 +25,18 @@ def main(argv=None):
         print(yaml_string)
 
 
-def _read_config():
-    config = configparser.ConfigParser()
-    file_dir = os.path.dirname(os.path.abspath(__file__))
+def create_yaml_spec(descriptor_contents: Dict, args, environment_info: EnvironmentInfo):
+    """
+    Creates the YAML spec file corresponding to a descriptor passed as parameter
+    :param descriptor_contents: dict containing the parsed descriptor
+    :param environment_info: contains BAI information, such as availability zones, etc
+    :return: The yaml string for the given descriptor
+    """
+    descriptor_config = DescriptorSettings(**args)
 
-    global_config = config.read(os.path.join(file_dir, 'config.ini'))
-    descriptor_config = DescriptorSettings(**global_config['descriptor-config'])
-
-    return global_config, descriptor_config
+    descriptor = Descriptor(descriptor_contents, descriptor_config)
+    bai_config = create_bai_config(descriptor, args, environment_info=environment_info)
+    return bai_config.dump_yaml_string()
 
 
 if __name__ == '__main__':
