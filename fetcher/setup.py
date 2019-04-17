3d94bb33ec8811cece4d9472ffd2efc0e9a276fb
Jose Luis Contreras <jlcont@amazon.com>
2019-04-17 16:03:55 +0200

    [Fetcher] Addressed comments on KafkaUtils refactor

--- fetcher/setup.py
+++ fetcher/setup.py
@@ -7,13 +7,13 @@ from setuptools import find_packages, setup
 from os.path import join, abspath, dirname
 
 here = abspath(dirname(__file__))
-bai_project_path = abspath(join(here, ".."))
+bai_project_path = abspath(join(here, os.pardir))
 
 setup(
     name='fetcher_dispatcher',
     url='https://github.com/MXNetEdge/benchmark-ai',
     package_dir={'': 'src'},
-    packages=find_packages("src") + [join(bai_project_path, "kafka-utils/src")],
+    packages=find_packages("src") + [join(bai_project_path, "kafka-utils", "src")],
     include_package_data=True,
     entry_points={
         "console_scripts": ["fetcher_dispatcher=fetcher_dispatcher.__main__:main"]
