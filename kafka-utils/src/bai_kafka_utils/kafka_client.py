638af5c6d4d9c99fa2659dbbb7bd444a0b457971
stsukrov <stsukrov@amazon.com>
2019-04-24 18:18:20 +0200

    Good intermediate

--- kafka-utils/src/bai_kafka_utils/kafka_client.py
+++ kafka-utils/src/bai_kafka_utils/kafka_client.py
@@ -1,24 +1,21 @@
 import logging
+from json import JSONDecodeError
+from typing import List
 
 import kafka
-from typing import List, Type
 
-from bai_kafka_utils.events import make_benchmark_event, BenchmarkPayload
+from bai_kafka_utils.events import BenchmarkEvent
 from bai_kafka_utils.utils import DEFAULT_ENCODING
 
 logger = logging.getLogger(__name__)
 
 
-def create_kafka_consumer(bootstrap_servers: List[str],
-                          group_id: str,
-                          topic: str,
-                          payload_type: Type[BenchmarkPayload]) -> kafka.KafkaConsumer:
+def create_kafka_consumer(bootstrap_servers: List[str], group_id: str, topic: str) -> kafka.KafkaConsumer:
     def json_deserializer(msg_value):
         try:
-
-            return make_benchmark_event(payload_type).from_json(msg_value.decode(DEFAULT_ENCODING))
-        except:
-            logger.exception("Failed to deserialize %s", msg_value)
+            return BenchmarkEvent.from_json(msg_value.decode(DEFAULT_ENCODING))
+        except JSONDecodeError:
+            logger.exception("Failed to deserialized %s", msg_value)
             return None
 
     return kafka.KafkaConsumer(topic, bootstrap_servers=bootstrap_servers, group_id=group_id,
