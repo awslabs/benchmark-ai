0eac88a36ba28b6d8bac00f81154366a0a6d77cc
Edison Gustavo Muenz <edisongustavo@gmail.com>
2019-04-25 15:19:09 +0200

    Add a CodeBuild "<project>-master" for each project that runs on the 'master' branch

--- ci/outputs.tf
+++ ci/outputs.tf
@@ -1,3 +1,3 @@
 output "ci-unit-tests-badge-url" {
-  value = "${zipmap(aws_codebuild_project.ci-unit-tests.*.name, aws_codebuild_project.ci-unit-tests.*.badge_url)}"
+  value = "${zipmap(aws_codebuild_project.ci-unit-tests.*.name, aws_codebuild_project.ci-unit-tests-master.*.badge_url)}"
 }
