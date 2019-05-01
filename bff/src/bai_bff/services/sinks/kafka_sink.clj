189a89fdbae1b92c4538db16752f4a970920360c
Gavin M. Bell <gavin.max.bell@gmail.com>
2019-05-01 15:10:35 +0200

    [WIP] *Made kafka message sending run in a diff thread using core.async
    Taking advantage of core.async to short circuit a bunch of "object"
    passing in the code.  This code does not build!  I have no f*^$$^#
    clue why.
    
    It gives:
    
    Syntax error compiling at (bai_bff/services/sinks/kafka_sink.clj:56:99).
    Exception in thread "main" Syntax error compiling at (bai_bff/services/sinks/kafka_sink.clj:56:99).
    	at clojure.lang.Compiler.analyze(Compiler.java:6808)
    	at clojure.lang.Compiler.analyze(Compiler.java:6745)
    ...
    	at clojure.lang.Var.applyTo(Var.java:705)
    	at clojure.main.main(main.java:37)
    Caused by: java.lang.RuntimeException: Unable to resolve symbol: event in this context
    	at clojure.lang.Util.runtimeException(Util.java:221)
    	at clojure.lang.Compiler.resolveIn(Compiler.java:7413)
    	at clojure.lang.Compiler.resolve(Compiler.java:7357)
    
    The only issue is that this is perfectly good code (according to me).
    When I put the channel call directly in the KafkaProducer args, it
    builds then.  Something is screwy.  I am making this commit just to
    keep a record of this issue.

--- bff/src/bai_bff/services/sinks/kafka_sink.clj
+++ bff/src/bai_bff/services/sinks/kafka_sink.clj
@@ -2,7 +2,10 @@
   (:require [bai-bff.services :refer [RunService Sink]]
             [bai-bff.utils.srv :refer :all]
             [bai-bff.services.eventbus :refer [send-event-channel-atom]]
-            [taoensso.timbre :as log])
+            [cheshire.core :as json]
+            [taoensso.timbre :as log]
+            [clojure.core.async :as a :refer [>! <! >!! <!! go chan buffer close! thread
+                                              alts! alts!! timeout]])
   (:import (org.apache.kafka.clients.producer KafkaProducer
                                               ProducerRecord)
            (org.apache.kafka.clients CommonClientConfigs)))
@@ -24,7 +27,7 @@
 
     ;NOTE: You should set "max.in.flight.requests.per.connection" to 1, esp. if "retries" is > 0 if you want to preserve order!
 
-(defrecord KafkaSinkService [config producer started?]
+(defrecord KafkaSinkService [config producer worker-thread started?]
   RunService
   (start! [this]
     (locking this
@@ -32,32 +35,37 @@
                  (not @started?))
         (log/info "Starting kafka producer! (sink)")
         (.assert-configured! config kafka-keys)
-        (let [kafka-config (doto (java.util.Properties.)
-                             (.put "bootstrap.servers" (get config :kafka-bootstrap-servers))
-                             (.put "acks"              (get config :kafka-acks "all"))
-                             (.put "retries"           (Integer/valueOf  (get config :kafka-retries 0)))
-                             (.put "max.in.flight.requests.per.connection"     (Integer/valueOf  (get config :kafka-max-in-flight-requests-per-connection 5)))
-                             (.put "batch.size"        (Integer/valueOf  (get config :kafka-batch-size 16384)))
-                             (.put "linger.ms"         (Integer/valueOf  (get config :kafka-linger-ms 0)))
-                             (.put "buffer.memory"     (Integer/valueOf  (get config :kafka-buffer-memory 33554432)))
-                             (.put "compression.type"  (get config :kafka-compression-type "none"))
-                             (.put "key.serializer"    (get config :kafka-key-serializer   "org.apache.kafka.common.serialization.StringDeserializer"))
-                             (.put "value.serializer"  (get config :kafka-value-serializer "org.apache.kafka.common.serialization.StringDeserializer"))
-                             )]
-          (reset! producer (KafkaProducer. kafka-config))
-          ;TODO (zoiks) - put in a loop here where I am reading from @send-event-channel-atom and passing to kafka.
-          (reset! started? true)))))
+        (reset! worker-thread
+                (Thread. (fn []
+                           (let [kafka-config (doto (java.util.Properties.)
+                                                (.put "bootstrap.servers" (get config :kafka-bootstrap-servers))
+                                                (.put "acks"              (get config :kafka-acks "all"))
+                                                (.put "retries"           (Integer/valueOf  (get config :kafka-retries 0)))
+                                                (.put "max.in.flight.requests.per.connection"     (Integer/valueOf  (get config :kafka-max-in-flight-requests-per-connection 5)))
+                                                (.put "batch.size"        (Integer/valueOf  (get config :kafka-batch-size 16384)))
+                                                (.put "linger.ms"         (Integer/valueOf  (get config :kafka-linger-ms 0)))
+                                                (.put "buffer.memory"     (Integer/valueOf  (get config :kafka-buffer-memory 33554432)))
+                                                (.put "compression.type"  (get config :kafka-compression-type "none"))
+                                                (.put "key.serializer"    (get config :kafka-key-serializer   "org.apache.kafka.common.serialization.StringDeserializer"))
+                                                (.put "value.serializer"  (get config :kafka-value-serializer "org.apache.kafka.common.serialization.StringDeserializer")))]
+
+                             (reset! producer (KafkaProducer. kafka-config))
+                             (while @started?
+                               (if-let [event (<!! @send-event-channel-atom)]
+                                 (log/info "Dispatching Message: <"event">")
+                                 (.send @producer (ProducerRecord. (get config :kafka-sink-topic) (json/generate-string event)))))
+                             (log/info "Shutdown Kafka producer (sender/sink)")
+                             (.close @producer)
+                             ))))
+        (log/info "Kafka Sink Service started...")
+        (reset! started? true)
+        (.start @worker-thread))))
   (stop! [_]
     (when @started?
-      (.close @producer)
-      ; XXX: Might make sense to reset atom first to prevent messages from
-      ; going through during shutdown
       (reset! started? false)))
   Sink
   (send! [_ message]
-    (when @started?
-      (.send @producer  (ProducerRecord. (get config :kafka-sink-topic)
-                                         (str message)))))
+    (log/info "<this is a no-op - remove me ["message"]>")) ; TODO use sink as a marker interface don't need this call directly.
   Object
   (toString [_] "<KafkaSinkService>"))
 
@@ -66,4 +74,4 @@
 
 
 (defn create-kafka-sink-service [config]
-  (KafkaSinkService. config (atom nil) (atom false)))
+  (KafkaSinkService. config (atom nil) (atom nil) (atom false)))
