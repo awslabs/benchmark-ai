52207c0c2a196ea4c88b063e1c91d30253ab03f8
Marco de Abreu <marco.g.abreu+github@gmail.com>
2019-05-03 13:28:46 +0200

    Merge in Chances stuff

--- baictl/run-baictl.sh
+++ baictl/run-baictl.sh
@@ -14,12 +14,15 @@ docker build -t benchmark-ai/aws-baictl-baictl-ecs .
 # Deploy/Update CloudFormation stack
 aws cloudformation deploy --stack-name baictl-ecs --template-file cf-baictl-ecs.yml --capabilities "CAPABILITY_IAM" --no-fail-on-empty-changeset
 
+# Extract deployment-specified values https://stackoverflow.com/questions/41628487/getting-outputs-from-aws-cloudformation-describe-stacks
+aws cloudformation describe-stacks --stack-name baictl-ecs
+
 # Push Docker image to ECR
 docker tag benchmark-ai/aws-baictl-baictl-ecs:latest 094741945095.dkr.ecr.us-west-2.amazonaws.com/benchmark-ai/aws-baictl-baictl-ecs:latest
 docker push 094741945095.dkr.ecr.us-west-2.amazonaws.com/benchmark-ai/aws-baictl-baictl-ecs:latest
 
 # Create run-once task
-aws ecs run-task --cluster baictl-ecs-cluster --launch-type FARGATE --task-definition arn:aws:ecs:us-west-2:094741945095:task-definition/baictl:1 --network-configuration '{"awsvpcConfiguration":{"subnets":["subnet-01f2b8c5ebad849dd","subnet-0fe18726b524db0fd"],"securityGroups":["sg-071cd497121b0e9ad"],"assignPublicIp":"ENABLED"}}'
+aws ecs run-task --cluster baictl-ecs-cluster --launch-type FARGATE --task-definition arn:aws:ecs:us-west-2:094741945095:task-definition/baictl:2 --network-configuration '{"awsvpcConfiguration":{"subnets":["subnet-0a6f397572fca14ab","subnet-032ea0be3d6f34b65"],"securityGroups":["sg-0dd8c65166734f4fd"],"assignPublicIp":"ENABLED"}}'
 # Stream logs ?
 
 # Ask whether CloudFormation stack should be destroyed again
