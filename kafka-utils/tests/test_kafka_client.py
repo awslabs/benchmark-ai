97e9346df6fd229b09ed0446ad99b26175b1e198
Jose Luis Contreras <jlcont@amazon.com>
2019-05-07 17:10:08 +0200

    [kafka] Added connections_max_idle_ms to avoid consumers/producers from closing

--- kafka-utils/tests/test_kafka_client.py
+++ kafka-utils/tests/test_kafka_client.py
@@ -4,7 +4,12 @@ import pytest
 
 import bai_kafka_utils
 from bai_kafka_utils.events import BenchmarkEvent, BenchmarkPayload
-from bai_kafka_utils.kafka_client import create_kafka_consumer, create_kafka_producer, WrongBenchmarkEventTypeException
+from bai_kafka_utils.kafka_client import (
+    create_kafka_consumer,
+    create_kafka_producer,
+    WrongBenchmarkEventTypeException,
+    MAX_IDLE_TIME_MS,
+)
 from bai_kafka_utils.utils import DEFAULT_ENCODING
 
 BOOTSTRAP_SERVERS = ["kafka_node"]
@@ -19,7 +24,11 @@ WRONG_SCHEMA_JSON = '{"foo":"bar"}'.encode(DEFAULT_ENCODING)
 def test_kafka_consumer_pass_through(mockKafkaConsumer):
     create_kafka_consumer(BOOTSTRAP_SERVERS, GROUP_ID, TOPIC, BenchmarkEvent)
     mockKafkaConsumer.assert_called_with(
-        TOPIC, bootstrap_servers=BOOTSTRAP_SERVERS, group_id=GROUP_ID, value_deserializer=ANY
+        TOPIC,
+        bootstrap_servers=BOOTSTRAP_SERVERS,
+        group_id=GROUP_ID,
+        value_deserializer=ANY,
+        connections_max_idle_ms=MAX_IDLE_TIME_MS,
     )
 
 
@@ -44,7 +53,9 @@ def test_kafka_consumer_handles_wrong_schema(mockKafkaConsumer):
 @patch.object(bai_kafka_utils.kafka_client.kafka, "KafkaProducer")
 def test_kafka_producer_pass_through(mockKafkaProducer):
     create_kafka_producer(BOOTSTRAP_SERVERS)
-    mockKafkaProducer.assert_called_with(bootstrap_servers=BOOTSTRAP_SERVERS, value_serializer=ANY)
+    mockKafkaProducer.assert_called_with(
+        bootstrap_servers=BOOTSTRAP_SERVERS, value_serializer=ANY, connections_max_idle_ms=MAX_IDLE_TIME_MS
+    )
 
 
 def test_wrong_event_type():
