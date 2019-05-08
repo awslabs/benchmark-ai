edbc67f994740d35c8dceb8fa5f30275c6bf45ce
Edison Gustavo Muenz <edisongustavo@gmail.com>
2019-05-08 15:05:01 +0200

    [watcher] Add environment variables

--- watcher/src/bai_watcher/args.py
+++ watcher/src/bai_watcher/args.py
@@ -7,17 +7,20 @@ from bai_watcher import SERVICE_NAME
 
 @dataclass
 class WatcherServiceConfig:
-    kubernetes_namespace: str = "default"
+    kubernetes_namespace_of_running_jobs: str = "default"
     kubeconfig: str = None
 
 
 def get_watcher_service_config(args) -> WatcherServiceConfig:
     parser = ArgParser(auto_env_var_prefix="", prog=SERVICE_NAME)
 
-    parser.add_argument("--kubernetes-namespace", default="default")
-    parser.add_argument("--kubeconfig")
+    parser.add_argument(
+        "--kubernetes-namespace-of-running-jobs", default="default", env_var="KUBERNETES_NAMESPACE_OF_RUNNING_JOBS"
+    )
+    parser.add_argument("--kubeconfig", env_var="KUBECONFIG")
 
     parsed_args, _ = parser.parse_known_args(args)
     return WatcherServiceConfig(
-        kubernetes_namespace=parsed_args.kubernetes_namespace, kubeconfig=parsed_args.kubeconfig
+        kubernetes_namespace_of_running_jobs=parsed_args.kubernetes_namespace_of_running_jobs,
+        kubeconfig=parsed_args.kubeconfig,
     )
