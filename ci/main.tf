c9716e6fd34e25357c2223f3690d68ea73344acc
Edison Gustavo Muenz <edisongustavo@gmail.com>
2019-05-14 14:42:45 +0200

    .

--- ci/main.tf
+++ ci/main.tf
@@ -213,3 +213,156 @@ resource "null_resource" "ci-unit-tests-filter" {
 //    filter_groups = "${jsonencode(local.filter_groups_master)}"
 //  }
 //}
+//
+//resource "aws_s3_bucket" "codepipeline_bucket" {
+//  bucket = "test-bucket"
+//  acl    = "private"
+//}
+//
+//resource "aws_iam_role" "codepipeline_role" {
+//  name = "test-role"
+//
+//  assume_role_policy = <<EOF
+//{
+//  "Version": "2012-10-17",
+//  "Statement": [
+//    {
+//      "Effect": "Allow",
+//      "Principal": {
+//        "Service": "codepipeline.amazonaws.com"
+//      },
+//      "Action": "sts:AssumeRole"
+//    }
+//  ]
+//}
+//EOF
+//}
+//
+//resource "aws_iam_role_policy" "codepipeline_policy" {
+//  name = "codepipeline_policy"
+//  role = "${aws_iam_role.codepipeline_role.id}"
+//
+//  policy = <<EOF
+//{
+//  "Version": "2012-10-17",
+//  "Statement": [
+//    {
+//      "Effect":"Allow",
+//      "Action": [
+//        "s3:GetObject",
+//        "s3:GetObjectVersion",
+//        "s3:GetBucketVersioning"
+//      ],
+//      "Resource": [
+//        "${aws_s3_bucket.codepipeline_bucket.arn}",
+//        "${aws_s3_bucket.codepipeline_bucket.arn}/*"
+//      ]
+//    },
+//    {
+//      "Effect": "Allow",
+//      "Action": [
+//        "codebuild:BatchGetBuilds",
+//        "codebuild:StartBuild"
+//      ],
+//      "Resource": "*"
+//    }
+//  ]
+//}
+//EOF
+//}
+//
+//data "aws_kms_alias" "s3kmskey" {
+//  name = "alias/myKmsKey"
+//}
+//
+//resource "aws_codepipeline" "codepipeline" {
+//  name     = "master"
+//  role_arn = "${aws_iam_role.codepipeline_role.arn}"
+//
+//  artifact_store {
+//    location = "${aws_s3_bucket.codepipeline_bucket.bucket}"
+//    type     = "S3"
+//
+//    encryption_key {
+//      id   = "${data.aws_kms_alias.s3kmskey.arn}"
+//      type = "KMS"
+//    }
+//  }
+//
+//  stage {
+//    name = "Source"
+//
+//    action {
+//      name             = "Source"
+//      category         = "Source"
+//      owner            = "ThirdParty"
+//      provider         = "GitHub"
+//      version          = "1"
+//      output_artifacts = ["source_output"]
+//
+//      configuration = {
+//        Owner  = "MXNetEdge"
+//        Repo   = "benchmark-ai"
+//        Branch = "master"
+//      }
+//    }
+//  }
+//
+//  stage {
+//    name = "Build"
+//
+//    action {
+//      name            = "Build"
+//      category        = "Build"
+//      owner           = "AWS"
+//      provider        = "CodeBuild"
+//      input_artifacts = ["source_output"]
+//      output_artifacts = ["build_output"]
+//      version         = "1"
+//
+//      configuration = {
+//        ProjectName = "test"
+//      }
+//    }
+//  }
+//
+////  stage {
+////    name = "Deploy"
+////
+////    action {
+////      name             = "Deploy"
+////      category         = "Deploy"
+////      owner            = "AWS"
+////      provider         = "CloudFormation"
+////      input_artifacts  = ["build_output"]
+////      version          = "1"
+////
+////      configuration {
+////        ActionMode     = "REPLACE_ON_FAILURE"
+////        Capabilities   = "CAPABILITY_AUTO_EXPAND,CAPABILITY_IAM"
+////        OutputFileName = "CreateStackOutput.json"
+////        StackName      = "MyStack"
+////        TemplatePath   = "build_output::sam-templated.yaml"
+////      }
+////    }
+////  }
+//}
+//
+//locals {
+//  codepipeline-stages = {
+//
+//  }
+//}
+//
+//# We need terraform 0.12 for dynamic number of stages in the CodePipeline
+//resource "null_resource" "codepipeline-build-stage" {
+//  count = "${length(var.projects)}"
+//  provisioner "local-exec" {
+//    command = "aws --region ${var.region} codebuild update-webhook --project-name ${self.triggers.project_name} --filter-groups '[${self.triggers.filter_groups}]'"
+//  }
+//
+//  triggers {
+//    project_name = "${element(aws_codebuild_webhook.ci-unit-tests.*.project_name, count.index)}"
+//    filter_groups = "${jsonencode(local.filter_groups_prs)}"
+//  }
+//}
