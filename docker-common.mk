592338ea0e09fd088bd094a8766856efeadf8095
Jose Luis Contreras <jlcont@amazon.com>
2019-05-16 12:26:08 +0200

    [deploy] Addressing comments on auto docker tags

--- docker-common.mk
+++ docker-common.mk
@@ -16,7 +16,7 @@ _pre_docker_package::
 	echo "Pre docker actions"
 
 _docker_package: _pre_docker_package
-	$(DOCKER) build .. -f ../Dockerfile-$(PROJECT) -t $(DOCKER_IMAGE_TAG) -t $(DOCKER_REPOSITORY):latest
+	$(DOCKER) build .. -f ../Dockerfile-$(PROJECT) -t $(DOCKER_IMAGE_TAG)
 
 _post_docker_package:: _docker_package
 	echo "Post docker actions"
@@ -28,12 +28,9 @@ publish: package docker_publish
 docker_publish: docker_package
 	echo "Publishing $(DOCKER_IMAGE_TAG)"
 	$(DOCKER) push $(DOCKER_IMAGE_TAG)
-	# Always push to latest as well as the generated tag
-	$(DOCKER) push $(DOCKER_REPOSITORY):latest
 
 _deploy_venv:
 	conda env update --file ../deploy-environment.yml --prune --name $(DEPLOY_ENV_NAME)
-
 
 deploy: publish k8s_deploy
 
@@ -43,7 +40,7 @@ undeploy: k8s_undeploy
 # K8S deploy/undeploy
 #---------------------
 define fn_k8s_deploy
-	find ./deploy -name '*.yml' -exec sh -c "sed 's|@@DOCKER_IMAGE@@|$(DOCKER_IMAGE_TAG)|g' {} | $(KUBECTL) apply $(KUBECTL_FLAGS) -f -" \;
+	find ./deploy -name '*.yml' -exec sh -c "sed 's|@@DOCKER_IMAGE_TAG@@|$(DOCKER_IMAGE_TAG)|g' {} | $(KUBECTL) apply $(KUBECTL_FLAGS) -f -" \;
 endef
 
 define fn_k8s_undeploy
