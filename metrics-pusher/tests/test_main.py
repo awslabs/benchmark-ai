1c3067088dac3d81937ec4b0f747c33012c5f8fc
Edison Gustavo Muenz <edisongustavo@gmail.com>
2019-05-17 17:43:21 +0200

    [metrics-pusher] Add test_main

---
+++ metrics-pusher/tests/test_main.py
@@ -0,0 +1,12 @@
+from bai_metrics_pusher.args import InputValue
+
+
+def test_main(mocker):
+    mock_start_loop = mocker.patch("bai_metrics_pusher.loop.start_loop")
+    from bai_metrics_pusher.__main__ import main
+
+    main("--backend stdout --pod-namespace pod-namespace --pod-name pod-name ")
+
+    expected_input = InputValue(backend="stdout", pod_name="pod-name", pod_namespace="pod-namespace", backend_args={})
+
+    mock_start_loop.assert_called_with(expected_input)
