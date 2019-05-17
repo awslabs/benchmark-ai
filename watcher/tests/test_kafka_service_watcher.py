8fecf817b0e2efe69a16e7d47806f489d622c934
Edison Gustavo Muenz <edisongustavo@gmail.com>
2019-05-17 11:50:13 +0200

    [watcher] Fix `test_kafka_service_watcher` by mocking `kubernetes.config`
    If the host does not have a directory in `~/.bai`, then
    `test_create_service()` will fail.

--- watcher/tests/test_kafka_service_watcher.py
+++ watcher/tests/test_kafka_service_watcher.py
@@ -19,6 +19,11 @@ def kafka_service_config():
     )
 
 
+@pytest.fixture(autouse=True)
+def kubernetes_config(mocker):
+    return mocker.patch("kubernetes.config")
+
+
 def test_create_service(mocker, kafka_service_config):
     kafka_producer_class = mocker.patch.object(kafka, "KafkaProducer")
     kafka_consumer_class = mocker.patch.object(kafka, "KafkaConsumer")
@@ -32,16 +37,14 @@ def test_create_service(mocker, kafka_service_config):
     kafka_consumer_class.assert_called_once()
 
 
-def test_constructor_loads_kubernetes_config_with_inexistent_kubeconfig_file(mocker):
-    kubernetes_config = mocker.patch("kubernetes.config")
+def test_constructor_loads_kubernetes_config_with_inexistent_kubeconfig_file(kubernetes_config):
     service_config = WatcherServiceConfig(kubeconfig="inexistent-path")
     WatchJobsEventHandler(service_config)
     assert kubernetes_config.load_incluster_config.call_args_list == [call()]
     assert kubernetes_config.load_kube_config.call_args_list == []
 
 
-def test_constructor_loads_kubernetes_config_with_existing_kubeconfig_file(mocker, datadir):
-    kubernetes_config = mocker.patch("kubernetes.config")
+def test_constructor_loads_kubernetes_config_with_existing_kubeconfig_file(kubernetes_config, datadir):
     kubeconfig_filename = str(datadir / "kubeconfig")
     service_config = WatcherServiceConfig(kubeconfig=kubeconfig_filename)
     WatchJobsEventHandler(service_config)
