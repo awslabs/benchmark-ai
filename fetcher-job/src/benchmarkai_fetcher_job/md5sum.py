a22411ca3fbac00f5c089f643cac4d478b6afdd3
stsukrov <stsukrov@amazon.com>
2019-05-18 16:46:05 +0200

    Improved wording and comments

--- fetcher-job/src/benchmarkai_fetcher_job/md5sum.py
+++ fetcher-job/src/benchmarkai_fetcher_job/md5sum.py
@@ -2,13 +2,14 @@ import hashlib
 
 from typing import TextIO, NamedTuple
 
-S3_MAX_CHUNK = 8 * 1024 * 1024
+S3_MAX_CHUNK_BYTES = 8 * 1024 * 1024  # 8 MB
 
 DigestPair = NamedTuple("DigestPair", [("md5", str), ("s3_etag", str)])
 
 
-# Slightly optimized version
-def calculate_md5_and_etag(fp: TextIO, chunk_size=S3_MAX_CHUNK) -> DigestPair:
+# Calculates MD5 and ETag in a single run.
+# Hash state for ETag is created lazily, just for the files > 8Mb
+def calculate_md5_and_etag(fp: TextIO, chunk_size=S3_MAX_CHUNK_BYTES) -> DigestPair:
     fp.seek(0)
 
     global_hash = hashlib.md5()
