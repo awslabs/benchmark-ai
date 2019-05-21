b1eec55b93786f97f4db7c92e9fb280af24e46a0
Edison Gustavo Muenz <edisongustavo@gmail.com>
2019-05-21 16:13:55 +0200

    [metrics-pusher] Remove commented code

--- metrics-pusher/src/bai_metrics_pusher/backends/kafka_backend.py
+++ metrics-pusher/src/bai_metrics_pusher/backends/kafka_backend.py
@@ -52,9 +52,6 @@ class KafkaBackend:
 
     def __call__(self, metrics):
         now = datetime.datetime.utcnow()
-        # now = datetime.datetime(1970, 1, 1, hour=0, minute=0, second=1)
-        # epoch = datetime.datetime(1970, 1, 1)
-        # timestamp = int((now - epoch).total_seconds() * 1000)
         timestamp_in_millis = int(now.timestamp()) * 1000
         for metric_name, metric_value in metrics.items():
             metric_object = KafkaExporterMetric(
