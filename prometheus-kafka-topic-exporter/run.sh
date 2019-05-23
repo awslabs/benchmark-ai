467e837e54d38617f1bb9ffc97fb8cc0e06d5758
Marco de Abreu <marco.g.abreu+github@gmail.com>
2019-05-23 21:37:35 +0200

    Update documentation

--- prometheus-kafka-topic-exporter/run.sh
+++ prometheus-kafka-topic-exporter/run.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 set -ex
 
-# Fill in template
+# Fill in template - expects config to be mounted at /config/kafka-topic-exporter.properties
 sed  "s/@@KAFKA_BOOTSTRAP_SERVERS@@/$KAFKA_BOOTSTRAP_SERVERS/g" /config/kafka-topic-exporter.properties.template > /tmp/kafka-topic-exporter.properties
 
+# The default is to log to a local file - not that convenient when expecting output on stdout
 java -Dlog4j.configuration=file:///config/log4j.properties -jar /opt/kafka-topic-exporter.jar /tmp/kafka-topic-exporter.properties
