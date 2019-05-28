dd9e58aab9806963017366e1a10fc8dd776fb874
Jose Luis Contreras <jlcont@amazon.com>
2019-05-28 10:56:06 +0200

    Change logging format to include timestamp

--- fetcher-job/src/bai_fetcher_job/__main__.py
+++ fetcher-job/src/bai_fetcher_job/__main__.py
@@ -3,11 +3,12 @@ import os
 
 from bai_fetcher_job.args import FetcherJobConfig, get_fetcher_job_args
 from bai_fetcher_job.fetcher import retrying_fetch
+from bai_kafka_utils.utils import LOGGING_FORMAT
 
 
 def main(argv=None):
     cfg: FetcherJobConfig = get_fetcher_job_args(argv, os.environ)
-    logging.basicConfig(level=cfg.logging_level)
+    logging.basicConfig(level=cfg.logging_level, format=LOGGING_FORMAT)
 
     retrying_fetch(cfg)
 
--- kafka-utils/src/bai_kafka_utils/utils.py
+++ kafka-utils/src/bai_kafka_utils/utils.py
@@ -6,6 +6,8 @@ import string
 
 DEFAULT_ENCODING = encodings.utf_8.getregentry().name
 
+LOGGING_FORMAT = "%(asctime)s %(levelname)s: %(message)s"
+
 
 def id_generator(size=8, chars=string.ascii_lowercase + string.digits):
     return "".join(random.choice(chars) for _ in range(size))
