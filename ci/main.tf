2a405118e28414b50453042918f15464e32e4f71
Edison Gustavo Muenz <edisongustavo@gmail.com>
2019-05-29 11:36:36 +0200

    [ci] Add build of master

--- ci/main.tf
+++ ci/main.tf
@@ -41,93 +41,92 @@ provider "aws" {
   ] # bai-gamma@amazon.com
 }
 
+data "aws_caller_identity" "current" {}
+
+# Generic role for both CodeBuild / CodePipeline to access the S3 bucket with artifacts
+resource "aws_s3_bucket" "build-artifacts" {
+  bucket = "bai-build-artifacts-${data.aws_caller_identity.current.account_id}"
+  acl    = "private"
+}
+
+resource "aws_iam_policy" "build-artifacts" {
+  name = "bai-build-artifacts"
+  description = "Provides permissions for CodePipeline/CodeBuild to start builds and store artifacts"
+
+  policy = <<-EOF
+  {
+    "Version": "2012-10-17",
+    "Statement": [
+      {
+        "Effect":"Allow",
+        "Action": [
+          "s3:*"
+        ],
+        "Resource": [
+          "${aws_s3_bucket.build-artifacts.arn}",
+          "${aws_s3_bucket.build-artifacts.arn}/*"
+        ]
+      }
+    ]
+  }
+  EOF
+}
+
+
+# Codebuild specific stuff
 
 resource "aws_iam_role" "code-build-role" {
   name = "code-build-role"
 
-  assume_role_policy = <<EOF
-{
-  "Version": "2012-10-17",
-  "Statement": [
-    {
-      "Effect": "Allow",
-      "Principal": {
-        "Service": "codebuild.amazonaws.com"
-      },
-      "Action": "sts:AssumeRole"
-    }
-  ]
-}
-EOF
+  assume_role_policy = <<-EOF
+  {
+    "Version": "2012-10-17",
+    "Statement": [
+      {
+        "Effect": "Allow",
+        "Principal": {
+          "Service": "codebuild.amazonaws.com"
+        },
+        "Action": "sts:AssumeRole"
+      }
+    ]
+  }
+  EOF
 }
 
 resource "aws_iam_role_policy" "code-build-role-policy" {
+  name = "logs-permissions"
   role = aws_iam_role.code-build-role.name
 
-  policy = <<EOF
-{
-  "Version": "2012-10-17",
-  "Statement": [
-    {
-      "Effect": "Allow",
-      "Resource": [
-        "*"
-      ],
-      "Action": [
-        "logs:CreateLogGroup",
-        "logs:CreateLogStream",
-        "logs:PutLogEvents"
-      ]
-    }
-  ]
+  policy = <<-EOF
+  {
+    "Version": "2012-10-17",
+    "Statement": [
+      {
+        "Effect": "Allow",
+        "Resource": [
+          "*"
+        ],
+        "Action": [
+          "logs:CreateLogGroup",
+          "logs:CreateLogStream",
+          "logs:PutLogEvents"
+        ]
+      }
+    ]
+  }
+  EOF
 }
-EOF
+
+resource "aws_iam_role_policy_attachment" "codebuild-attachment" {
+  role = aws_iam_role.code-build-role.name
+  policy_arn = aws_iam_policy.build-artifacts.arn
 }
 
 resource "aws_codebuild_project" "ci-unit-tests" {
   count         = length(var.projects)
   name          = element(var.projects, count.index)
   description   = "Unit tests build of ${element(var.projects, count.index)}"
-  build_timeout = "10"
-  service_role  = aws_iam_role.code-build-role.arn
-  badge_enabled = true
-
-  artifacts {
-    type = "NO_ARTIFACTS"
-  }
-
-  environment {
-    compute_type = "BUILD_GENERAL1_SMALL"
-    image = lookup(
-      var.ci_docker_image,
-      element(var.projects, count.index),
-      var.ci_docker_image["default"],
-    )
-    type = "LINUX_CONTAINER"
-  }
-
-  source {
-    type            = "GITHUB"
-    location        = "https://github.com/MXNetEdge/benchmark-ai.git"
-    git_clone_depth = 1
-    auth {
-      type = "OAUTH"
-    }
-    buildspec           = "${element(var.projects, count.index)}/buildspec.yml"
-    report_build_status = true
-  }
-
-  tags = {
-    GithubRepo = "benchmark-ai"
-    GithubOrg  = "MXNetEdge"
-    Workspace  = terraform.workspace
-  }
-}
-
-resource "aws_codebuild_project" "ci-unit-tests-master" {
-  count         = length(var.projects)
-  name          = "${element(var.projects, count.index)}-master"
-  description   = "Unit tests master build of ${element(var.projects, count.index)}"
   build_timeout = "10"
   service_role  = aws_iam_role.code-build-role.arn
   badge_enabled = true
@@ -203,22 +202,3 @@ resource "null_resource" "ci-unit-tests-filter" {
     filter_groups = jsonencode(local.filter_groups_prs)
   }
 }
-
-// TODO: Enable webhooks for the `master` branch again. Right now we're hitting Github's limit of 20 webhooks, so they
-// are disabled for now. A good alternative would be to use CodePipeline for master, since we probably want to have it
-// deploying the latest changes from master as well.
-//resource "aws_codebuild_webhook" "ci-unit-tests-master" {
-//  count = "${length(var.projects)}"
-//  project_name = "${element(aws_codebuild_project.ci-unit-tests-master.*.name, count.index)}"
-//}
-//resource "null_resource" "ci-unit-tests-master-filter" {
-//  count = "${length(var.projects)}"
-//  provisioner "local-exec" {
-//    command = "aws --region ${var.region} codebuild update-webhook --project-name ${self.triggers.project_name} --filter-groups '[${self.triggers.filter_groups}]'"
-//  }
-//
-//  triggers {
-//    project_name = "${element(aws_codebuild_webhook.ci-unit-tests-master.*.project_name, count.index)}"
-//    filter_groups = "${jsonencode(local.filter_groups_master)}"
-//  }
-//}
