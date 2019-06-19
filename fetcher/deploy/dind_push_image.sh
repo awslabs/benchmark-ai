1e2deb607542de1a84f474efadb9bafa7d4ffc3f
Stanislav Tsukrov <46340895+stsukrov@users.noreply.github.com>
2019-06-19 15:14:54 +0200

    Move fetcher to kind (#421)
    * Move fetcher to kind
    
    * Repush

--- fetcher/deploy/dind_push_image.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-# Hack necessary to deploy image to a container running docker, without pushing it to a repo.
-# Later solution can be a local repository service.
-
-CONTAINER=$1
-IMAGE=$2
-
-docker save ${IMAGE} | docker exec -i ${CONTAINER} docker load
