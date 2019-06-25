54b3b5c45ea0c1d04c51d4b2b651119241a45b31
Edison Gustavo Muenz <edisongustavo@gmail.com>
2019-06-25 16:45:34 +0200

    [blackbox-tests] Remove StatusInfo

--- blackbox-tests/tests/test_run_benchmarks.py
+++ blackbox-tests/tests/test_run_benchmarks.py
@@ -6,7 +6,7 @@ from contextlib import contextmanager
 import subprocess
 import tempfile
 
-from bai_kafka_utils.events import Status
+from bai_kafka_utils.events import Status, StatusMessageBenchmarkEvent
 from typing import Iterable, Iterator
 from collections import namedtuple
 from pathlib import Path
@@ -48,17 +48,14 @@ def create_client():
                 port_forward_process.kill()
 
 
-StatusInfo = namedtuple("StatusInfo", ("message_id", "status", "message", "service"))
-
-
-def generate_status_infos(
+def generator_status_messages(
     bai_client,
     action_id,
     *,
     sleep_between_status_checks=datetime.timedelta(milliseconds=500),
     timeout=datetime.timedelta(minutes=10),
     callback_on_every_status_check=lambda: None,
-) -> Iterator[StatusInfo]:
+) -> Iterator[StatusMessageBenchmarkEvent]:
     """
     Generates `StatusInfo` objects by querying the Status API of BFF.
 
@@ -75,19 +72,16 @@ def generate_status_infos(
     deadline = datetime.datetime.utcnow() + timeout
     index_of_last_status = 0
     while deadline > datetime.datetime.utcnow():
-        status_events = bai_client.status(action_id)
+        status_messages = bai_client.status(action_id)
         callback_on_every_status_check()
 
-        status_infos = [
-            StatusInfo(event.message_id, event.status, event.message, event.visited[-1].svc) for event in status_events
-        ]
-        if len(status_infos) == 0:
+        if len(status_messages) == 0:
             continue
 
-        status_infos_to_print = status_infos[index_of_last_status:]
+        status_infos_to_print = status_messages[index_of_last_status:]
         for status_info in status_infos_to_print:
             yield status_info
-        index_of_last_status = len(status_infos)
+        index_of_last_status = len(status_messages)
         time.sleep(sleep_between_status_checks.total_seconds())
     else:
         raise TimeoutError()
@@ -97,17 +91,15 @@ def wait_for_benchmark_completion(bai_client, action_id):
     def print_progress():
         sys.stdout.write(".")
 
-    status_events = generate_status_infos(bai_client, action_id, callback_on_every_status_check=print_progress)
-    for status_info in status_events:
+    status_messages = generator_status_messages(bai_client, action_id, callback_on_every_status_check=print_progress)
+    for status_message in status_messages:
         print()
-        print(f"Benchmark Status: [{status_info.service}] - {status_info.status}: {status_info.message}")
-        if status_info.service == "watcher" and status_info.status in [Status.FAILED, Status.SUCCEEDED]:
-            print(f"Benchmark finished: {status_info.status}")
+        service = status_message.visited[-1].svc
+        print(f"Benchmark Status: [{service}] - {status_message.status}: {status_message.message}")
+        if service == "watcher" and status_message.status in [Status.FAILED, Status.SUCCEEDED]:
+            print(f"Benchmark finished: {status_message.status}")
             return
         sys.stdout.flush()
-
-
-ServiceAndStatus = namedtuple("T", ("visited_service", "status"))
 
 
 def unique(objects: Iterable) -> Iterable:
@@ -147,16 +139,16 @@ def test_sample_benchmarks(descriptor_filename):
         action_id = client.submit(full_path)
         print(f"action_id={action_id}")
         wait_for_benchmark_completion(client, action_id)
-        status_events = client.status(action_id)
+        status_messages = client.status(action_id)
     print("#" * 120)
-    print("# Status events (for debugging)")
+    print("# Status messages (for debugging)")
     print("#" * 120)
-    print(status_events)
+    print(status_messages)
 
-    visited_services = [event.visited[-1].svc for event in status_events]
-    status_values = [event.status for event in status_events]
-
-    events = (ServiceAndStatus(*tuple) for tuple in zip(visited_services, status_values))
+    ServiceAndStatus = namedtuple("T", ("visited_service", "status"))
+    events = (
+        ServiceAndStatus(visited_service=message.visited[-1].svc, status=message.status) for message in status_messages
+    )
     events = unique(events)
     events = list(events)
 
@@ -171,5 +163,5 @@ def test_sample_benchmarks(descriptor_filename):
     ]
 
     # All visited services
-    visited_services = [visited.svc for visited in status_events[-1].visited]
+    visited_services = [visited.svc for visited in status_messages[-1].visited]
     assert visited_services == ["bai-client-python", "bai-bff", "fetcher-dispatcher", "executor", "watcher"]
