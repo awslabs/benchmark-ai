b79a59177c9c08067c8a5784f90eeb991c7e26bc
Gavin M. Bell <gavin.max.bell@gmail.com>
2019-06-25 18:07:51 +0200

    [bff] [WIP] Sewing in S3 writing of scripts...

--- bff/src/bai_bff/http_api.clj
+++ bff/src/bai_bff/http_api.clj
@@ -28,6 +28,8 @@
 
 (defn dispatch-submit-job [request body]
   (try
+    (if-let [script-file-map (some-> request :params :script)]
+      (eventbus/scripts->s3 script-file-map))
     (let [message-body (json/parse-string body true)]
       (log/debug "Printing request")
       (log/debug request)
