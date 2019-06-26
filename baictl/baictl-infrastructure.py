b360b7cb32cd38ad4618e383844cb41987d228e0
Chance Bair <chancebair@gmail.com>
2019-06-26 11:46:51 +0200

    Wait for ECS Exit Code (#457)

--- baictl/baictl-infrastructure.py
+++ baictl/baictl-infrastructure.py
@@ -29,11 +29,14 @@ def main():
     parser.add_argument("mode", help="create or destroy", nargs="?", choices=("create", "destroy"))
     parser.add_argument("--debug", help="verbose debug output", action="store_true", default=False)
     args = parser.parse_args()
-
-    logging.getLogger().setLevel(logging.DEBUG) if args.debug else logging.getLogger().setLevel(logging.INFO)
-
     if not args.mode:
         raise ValueError('Please pass either "create" or "destroy".')
+
+    logger = logging.getLogger()
+    log_handler = logging.StreamHandler()
+    log_handler.setFormatter(logging.Formatter("%(asctime)s- %(levelname)s - %(message)s"))
+    logger.addHandler(log_handler)
+    logger.setLevel(logging.DEBUG) if args.debug else logging.getLogger().setLevel(logging.INFO)
 
     config = load_config(CONFIG_YAML_PATH)
 
@@ -46,12 +49,20 @@ def main():
         cloudformation_yaml_path=CLOUDFORMATION_YAML_PATH,
         baictl_command=generate_baictl_command(config=config, mode=args.mode),
     )
-    cloudwatch_log_group = cloudformation_output['BaictlLogGroupName']
+    cloudwatch_log_group = cloudformation_output["BaictlLogGroupName"]
     docker_cli, docker_registry = login_ecr(boto_session=boto_session)
     publish_docker_image(docker_cli=docker_cli, docker_tag=docker_tag, docker_registry=docker_registry)
-    ecs_task_response = run_ecs_task(boto_session=boto_session, cloudformation_output=cloudformation_output, ecs_cluster_name=ECS_CLUSTER_NAME)
-    cloudwatch_log_stream = parse_ecs_response(ecs_task_response)
-    get_cloudwatch_logs(boto_session=boto_session, cloudwatch_log_group=cloudwatch_log_group, cloudwatch_log_stream=cloudwatch_log_stream, aws_region=config.get_aws_region())
+    ecs_task_response = run_ecs_task(
+        boto_session=boto_session, cloudformation_output=cloudformation_output, ecs_cluster_name=ECS_CLUSTER_NAME
+    )
+    cloudwatch_log_stream, task_arn = parse_ecs_response(ecs_task_response)
+    get_cloudwatch_logs(
+        boto_session=boto_session,
+        cloudwatch_log_group=cloudwatch_log_group,
+        cloudwatch_log_stream=cloudwatch_log_stream,
+        aws_region=config.get_aws_region(),
+    )
+    check_ecs_task(boto_session=boto_session, ecs_cluster_name=ECS_CLUSTER_NAME, task_arn=task_arn)
     destroy_cloudformation()
 
 
@@ -203,8 +214,8 @@ def _cloudformation_stack_exists(cloudformation_client, stack_name):
 
 
 def publish_docker_image(docker_cli, docker_tag, docker_registry):
-    logging.info("Publishing docker image")
-    logging.info("Publish should take ~10 minutes depending on your internet speed")
+    logging.info("Publishing docker image, this might take ~15 minutes")
+
     output = docker_cli.push(
         docker_tag,
         stream=True,
@@ -224,6 +235,11 @@ def run_ecs_task(boto_session, cloudformation_output, ecs_cluster_name):
     logging.info("Executing infrastructure build on AWS Elastic Container Service")
     ecs_client = boto_session.client("ecs")
 
+    # Check that the task is not currently running
+    if len(ecs_client.list_tasks(cluster=ecs_cluster_name)["taskArns"]) != 0:
+        logging.error("ECS cluster {} has ongoing tasks".format(ecs_cluster_name))
+        raise Exception("Aborting!  There is a task already creating Anubis infrastructure!")
+
     logging.info("Running ECS Task to create infrastructure")
     resp = ecs_client.run_task(
         cluster=ecs_cluster_name,
@@ -237,8 +253,54 @@ def run_ecs_task(boto_session, cloudformation_output, ecs_cluster_name):
             }
         },
     )
-    
+
     return resp
+
+
+class ECS_Status:
+    running = ["RUNNING", "PENDING"]
+    complete = ["STOPPED", "DEPROVISIONING"]
+
+
+def check_ecs_task(boto_session, ecs_cluster_name, task_arn):
+    ecs_client = boto_session.client("ecs")
+    first_log_line = True
+    ecs_wait_time = 5
+
+    while True:
+        response = ecs_client.describe_tasks(cluster=ecs_cluster_name, tasks=[task_arn])
+        ecs_task = response["tasks"][0]
+        task_status = ecs_task["lastStatus"]
+        failures = response["failures"]
+
+        if task_status in ECS_Status.running:
+            if first_log_line:
+                logging.info("Waiting for ECS task to finish...")
+                first_log_line = False
+            else:
+                logging.debug("Still waiting for ECS task to finish")
+            time.sleep(ecs_wait_time)
+        elif task_status in ECS_Status.complete:
+            container_status = ecs_task["containers"][0]["exitCode"]
+
+            if container_status != 0:
+                logging.error(
+                    "Anubis infrastructure creation failed with exit code {}.  Check cloudwatch log link above for details.".format(
+                        container_status
+                    )
+                )
+                break
+            elif len(failures) != 0:
+                for failure in failures:
+                    logging.error("Failure on RESOURCE: {} REASON: {}".format(failure["arn"], failure["reason"]))
+                raise Exception("Failure(s) in ECS task to build infrastructure")
+            else:
+                logging.info("Complete!  Anubis infrastructure is ready")
+                logging.debug("ECS Task Response: {}".format(response))
+                break
+        else:
+            logging.error("Unable to get task return status \nResponse: {}".format(response))
+            raise Exception("Failure to return ECS Task status")
 
 
 def destroy_cloudformation():
@@ -248,36 +310,41 @@ def destroy_cloudformation():
 def get_cloudwatch_logs(boto_session, cloudwatch_log_group, cloudwatch_log_stream, aws_region):
     logs_client = boto_session.client("logs")
     LOG_STREAM_INCREMENT_SECONDS = 10
-    logging.info("Waiting for logs, this should take less than 120 seconds")
+    logging.info("Waiting for logs, this should take less than 60 seconds")
 
     # Wait 5 minutes for cloudwatch log stream to show up
     for wait_seconds in range(0, LOG_STREAM_WAIT_SECONDS, LOG_STREAM_INCREMENT_SECONDS):
         log_streams = logs_client.describe_log_streams(
-            logGroupName=cloudwatch_log_group,
-            orderBy='LastEventTime',
-            descending=True
-        )['logStreams']
+            logGroupName=cloudwatch_log_group, orderBy="LastEventTime", descending=True
+        )["logStreams"]
 
         log_streams_names = []
         for stream in log_streams:
-            log_streams_names.append(stream['logStreamName'])
+            log_streams_names.append(stream["logStreamName"])
 
         if cloudwatch_log_stream in log_streams_names:
-            logging.info("Cloudwatch log for run here: https://console.aws.amazon.com/cloudwatch/home?region={}#logEventViewer:group={};stream={}".format(aws_region,cloudwatch_log_group, cloudwatch_log_stream))
+            logging.info(
+                "Cloudwatch log for run here: https://console.aws.amazon.com/cloudwatch/home?region={}#logEventViewer:group={};stream={}".format(
+                    aws_region, cloudwatch_log_group, cloudwatch_log_stream
+                )
+            )
             break
-        logging.info("Waited {} seconds for Cloudwatch log stream...".format(wait_seconds))
+        logging.debug("Waited {} seconds for Cloudwatch log stream...".format(wait_seconds))
         time.sleep(LOG_STREAM_INCREMENT_SECONDS)
     else:
         logging.error("Can not find Cloudwatch log Stream {} -> {}".format(cloudwatch_log_group, cloudwatch_log_stream))
 
     # TODO: Stream Logs?
 
+
 def parse_ecs_response(ecs_task_response):
-    ecs_prefix_name = ecs_task_response['tasks'][0]['overrides']['containerOverrides'][0]['name']
-    ecs_container_name = ecs_task_response['tasks'][0]['containers'][0]['name']
-    ecs_task_id = ecs_task_response['tasks'][0]['taskArn'].split("/")[-1]
+    ecs_task = ecs_task_response["tasks"][0]
+    ecs_prefix_name = ecs_task["overrides"]["containerOverrides"][0]["name"]
+    ecs_container_name = ecs_task["containers"][0]["name"]
+    ecs_task_id = ecs_task["taskArn"].split("/")[-1]
     cloudwatch_log_stream = ecs_prefix_name + "/" + ecs_container_name + "/" + ecs_task_id
-    return cloudwatch_log_stream
+    task_arn = ecs_task["taskArn"]
+    return cloudwatch_log_stream, task_arn
 
 
 if __name__ == "__main__":
