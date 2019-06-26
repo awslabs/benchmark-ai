74490b4c2f5a9751718e94ae335d1ef7449e381c
stsukrov <stsukrov@amazon.com>
2019-06-26 11:42:51 +0200

    And fix it

--- fetcher/tests/fetcher_dispatcher/test_fetcher_dispatcher_service.py
+++ fetcher/tests/fetcher_dispatcher/test_fetcher_dispatcher_service.py
@@ -251,5 +251,5 @@ def test_create_data_set_manager(
 @patch.object(fetcher_dispatcher_service, "DataSetManager", autospec=True)
 def test_cmd_object(mockDataSetManager):
     cmd_object = DataSetCmdObject(mockDataSetManager)
-    cmd_object.delete("CLIENT_ID", "ACTION_ID")
+    cmd_object.cancel("CLIENT_ID", "ACTION_ID")
     mockDataSetManager.cancel.assert_called_once_with("CLIENT_ID", "ACTION_ID")
