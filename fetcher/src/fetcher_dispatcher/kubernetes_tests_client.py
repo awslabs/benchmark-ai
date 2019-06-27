05842ef930ecbf169ff807a3fbaf0f8d940f7f9d
stsukrov <stsukrov@amazon.com>
2019-06-27 18:03:01 +0200

    Timeout for wait for pod

--- fetcher/src/fetcher_dispatcher/kubernetes_tests_client.py
+++ fetcher/src/fetcher_dispatcher/kubernetes_tests_client.py
@@ -1,5 +1,6 @@
 import logging
 
+import timeout_decorator
 from time import sleep
 
 import kubernetes
@@ -13,10 +14,14 @@ logger = logging.getLogger(__name__)
 
 class KubernetesTestUtilsClient:
     # Utilities to use in integration tests.
+    # All of them expect pod to be at least in Pending state.
+    # So no super long timeouts are necessary for any environment
     def __init__(self, api_client: kubernetes.client.ApiClient, service: str):
         self.batch_api_instance = kubernetes.client.BatchV1Api(api_client)
         self.core_api_instance = kubernetes.client.CoreV1Api(api_client)
         self.service = service
+
+    DEFAULT_TIMEOUT_SECONDS = 10
 
     def is_pod_present(self, namespace: str, client_id: str, action_id: str):
         label_selector = KubernetesDispatcher.get_label_selector(self.service, client_id, action_id)
@@ -30,21 +35,25 @@ class KubernetesTestUtilsClient:
         pods: V1PodList = self.batch_api_instance.list_namespaced_job(namespace, label_selector=label_selector)
         return bool(pods.items)
 
+    @timeout_decorator.timeout(DEFAULT_TIMEOUT_SECONDS)
     def wait_for_pod_exists(self, namespace: str, client_id: str, action_id: str):
         while not self.is_pod_present(namespace, client_id, action_id):
             logger.info("pod doesn't exist yet")
             sleep(1)
 
+    @timeout_decorator.timeout(DEFAULT_TIMEOUT_SECONDS)
     def wait_for_pod_not_exists(self, namespace: str, client_id: str, action_id: str):
         while self.is_pod_present(namespace, client_id, action_id):
             logger.info("pod still exists")
             sleep(1)
 
+    @timeout_decorator.timeout(DEFAULT_TIMEOUT_SECONDS)
     def wait_for_job_exists(self, namespace: str, client_id: str, action_id: str):
         while not self.is_job_present(namespace, client_id, action_id):
             logger.info("job doesn't exist yet")
             sleep(1)
 
+    @timeout_decorator.timeout(DEFAULT_TIMEOUT_SECONDS)
     def wait_for_job_not_exists(self, namespace: str, client_id: str, action_id: str):
         while self.is_job_present(namespace, client_id, action_id):
             logger.info("job still exists")
