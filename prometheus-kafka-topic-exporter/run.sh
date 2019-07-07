d341c1b362df5764f5f08262b6db8e9f7a050e7e
Marco de Abreu <marcoabreu@users.noreply.github.com>
2019-07-07 03:51:02 +0200

    Add scrape endpoint for Prometheus Kafka Exporter (#553)
    * Add scrape endpoint for Prometheus Kafka Exporter
    
    * Add pull policy and improve kafka topic creation

--- prometheus-kafka-topic-exporter/run.sh
+++ prometheus-kafka-topic-exporter/run.sh
@@ -2,7 +2,17 @@
 set -ex
 
 # Fill in template - expects config to be mounted at /config/kafka-topic-exporter.properties
-sed  "s/@@KAFKA_BOOTSTRAP_SERVERS@@/$KAFKA_BOOTSTRAP_SERVERS/g" /config/kafka-topic-exporter.properties.template > /tmp/kafka-topic-exporter.properties
+sed  "s/@@KAFKA_BOOTSTRAP_SERVERS@@/$KAFKA_BOOTSTRAP_SERVERS/g" /config/kafka-topic-exporter.properties.template |\
+sed  "s/@@METRICS_CONSUMER_TOPIC@@/$METRICS_CONSUMER_TOPIC/g" /config/kafka-topic-exporter.properties.template \
+> /tmp/kafka-topic-exporter.properties
+
+# Create Kafka Topic we're consuming
+/opt/kafka/bin/kafka-topics.sh --create \
+  --if-not-exists \
+  --zookeeper ${KAFKA_ZOOKEEPER_SERVERS} \
+  --replication-factor ${KAFKA_REPLICATION_FACTOR} --partitions ${KAFKA_PARTITIONS} \
+  --topic ${METRICS_CONSUMER_TOPIC} \
+  || echo "Topic might already exist"
 
 # The default is to log to a local file - not that convenient when expecting output on stdout
 java -Dlog4j.configuration=file:///config/log4j.properties -jar /opt/kafka-topic-exporter.jar /tmp/kafka-topic-exporter.properties
