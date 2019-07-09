8905a543c79997bfd84c1c29f1213c31fc94edba
Gavin M. Bell <gavin.max.bell@gmail.com>
2019-07-09 13:41:49 +0200

    [bff] S3 Policy/Roles/Permissions Oh My!!!! (for writing to scripts-exchange bucket

--- baictl/drivers/aws/cluster/scripts_exchange.tf
+++ baictl/drivers/aws/cluster/scripts_exchange.tf
@@ -8,44 +8,42 @@ resource "aws_s3_bucket" "scripts-exchange" {
   }
 }
 
-resource "aws_iam_policy" "scripts-exchange-policy" {
-  name        = "scripts-exchange-policy"
-  path        = "/"
-  description = "Worker's policy for the bucket"
+resource "aws_iam_role_policy" "bai-bff-pod-role-s3-policy" {
+  name = "s3-permissions"
+  role = aws_iam_role.kube2iam-bff-pod-role.name
 
-  policy = <<EOF
-{
-  "Version": "2012-10-17",
-  "Statement": [
-    {
-      "Effect": "Allow",
-      "Action": [
-        "s3:ListBucket"
-      ],
-      "Resource": "${aws_s3_bucket.scripts-exchange.arn}"
-    },
-    {
-      "Effect": "Allow",
-      "Action": [
-        "s3:PutObject",
-        "s3:GetObject",
-
-        "s3:PutObjectTagging",
-        "s3:GetObjectTagging",
-
-        "s3:GetObjectVersion",
-
-        "s3:GetObjectVersionTagging",
-        "s3:PutObjectVersionTagging"
-      ],
-      "Resource": "${aws_s3_bucket.scripts-exchange.arn}/*"
-    }
-  ]
-}
-EOF
+  policy = <<-EOF
+  {
+    "Version": "2012-10-17",
+    "Statement": [
+      {
+        "Effect": "Allow",
+        "Resource": [
+          "${aws_s3_bucket.scripts-exchange.arn}"
+        ],
+        "Action": [
+          "s3:*"
+        ]
+      }
+    ]
+  }
+  EOF
 }
 
-resource "aws_iam_role_policy_attachment" "scripts_exchange_policy_attachment" {
-  policy_arn = "${aws_iam_policy.scripts-exchange-policy.arn}"
-  role       = "${module.eks.worker_iam_role_name}"
+resource "aws_iam_role" "kube2iam-bff-pod-role" {
+  name = "bai-bff-pod-role"
+  assume_role_policy = <<-EOF
+  {
+    "Version": "2012-10-17",
+    "Statement": [
+      {
+        "Effect": "Allow",
+        "Principal": {
+          "AWS": "${module.eks.worker_iam_role_arn}"
+        },
+        "Action": "sts:AssumeRole"
+      }
+    ]
+  }
+  EOF
 }
