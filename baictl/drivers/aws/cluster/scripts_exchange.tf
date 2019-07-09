3166a2895551c405549388290627cfe63d2f8a1b
Edison Gustavo Muenz <edisongustavo@gmail.com>
2019-07-09 14:24:47 +0200

    [baictl] Fix policies for scripts_exchange.tf

--- baictl/drivers/aws/cluster/scripts_exchange.tf
+++ baictl/drivers/aws/cluster/scripts_exchange.tf
@@ -10,40 +10,40 @@ resource "aws_s3_bucket" "scripts-exchange" {
 
 resource "aws_iam_role_policy" "bai-bff-pod-role-s3-policy" {
   name = "s3-permissions"
-  role = aws_iam_role.kube2iam-bff-pod-role.name
+  role = "${aws_iam_role.kube2iam-bff-pod-role.name}"
 
-  policy = <<-EOF
-  {
-    "Version": "2012-10-17",
-    "Statement": [
-      {
-        "Effect": "Allow",
-        "Resource": [
-          "${aws_s3_bucket.scripts-exchange.arn}"
-        ],
-        "Action": [
-          "s3:*"
-        ]
-      }
-    ]
-  }
-  EOF
+  policy = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Effect": "Allow",
+      "Resource": [
+        "${aws_s3_bucket.scripts-exchange.arn}"
+      ],
+      "Action": [
+        "s3:*"
+      ]
+    }
+  ]
+}
+EOF
 }
 
 resource "aws_iam_role" "kube2iam-bff-pod-role" {
   name = "bai-bff-pod-role"
-  assume_role_policy = <<-EOF
-  {
-    "Version": "2012-10-17",
-    "Statement": [
-      {
-        "Effect": "Allow",
-        "Principal": {
-          "AWS": "${module.eks.worker_iam_role_arn}"
-        },
-        "Action": "sts:AssumeRole"
-      }
-    ]
-  }
-  EOF
+  assume_role_policy = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Effect": "Allow",
+      "Principal": {
+        "AWS": "${module.eks.worker_iam_role_arn}"
+      },
+      "Action": "sts:AssumeRole"
+    }
+  ]
+}
+EOF
 }
