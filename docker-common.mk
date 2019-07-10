39ea0c989a6870a3bf413c4bf02c926599e27e97
Edison Gustavo Muenz <edisongustavo@gmail.com>
2019-07-10 13:17:39 +0200

    Rename LOCAL_DEPLOY => LOCAL_PUBLISH

--- docker-common.mk
+++ docker-common.mk
@@ -11,7 +11,7 @@ DOCKER_IMAGE_LABEL ?= $(COMMIT_SHORT_HASH)
 DOCKER_IMAGE_TAG = $(DOCKER_REPOSITORY):$(DOCKER_IMAGE_LABEL)
 
 # Default local action - don't push
-LOCAL_DEPLOY = echo "Skipping local publishing step - use local docker repo"
+LOCAL_PUBLISH = echo "Skipping local publishing step - use local docker repo"
 
 # package is a high level command while docker_package can be executed separately
 package: build docker_package
@@ -31,4 +31,4 @@ publish: docker_publish
 
 docker_publish: docker_package
 	echo "Publishing $(DOCKER_IMAGE_TAG)"
-	[[ "$(STAGE)" == "local" ]] && $(LOCAL_DEPLOY) $(DOCKER_IMAGE_TAG) || $(DOCKER) push $(DOCKER_IMAGE_TAG)
+	[[ "$(STAGE)" == "local" ]] && $(LOCAL_PUBLISH) $(DOCKER_IMAGE_TAG) || $(DOCKER) push $(DOCKER_IMAGE_TAG)
