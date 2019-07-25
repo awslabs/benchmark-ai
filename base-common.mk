121984ef6b6af1e26749fabe5b4c85e9a7a4bd22
Jose Luis Contreras <jlcont@amazon.com>
2019-07-25 10:48:13 +0200

    [makefiles] Set build as the default target for make in base-common.mk

--- base-common.mk
+++ base-common.mk
@@ -4,7 +4,13 @@ CONDA_ROOT = $(shell conda info --root)
 CONDA_ENV_BIN = $(CONDA_ROOT)/envs/$(ENV_NAME)/bin
 export PATH := $(CONDA_ENV_BIN):$(PATH)
 
+.DEFAULT_GOAL := build
+
 deploy:
 	echo "Nothing to deploy for $(PROJECT)"
+
 undeploy:
 	echo "Nothing to undeploy for $(PROJECT)"
+
+build:
+	echo "Nothing to build for $(PROJECT)"
