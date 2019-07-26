614f57ab8129aa986ad7984eb331afa902206fbc
Chance Bair <chancebair@gmail.com>
2019-07-26 13:58:13 +0200

    Fix prometheus install (#641)

--- baictl/drivers/aws/cluster/eks.tf
+++ baictl/drivers/aws/cluster/eks.tf
@@ -2,9 +2,9 @@ locals {
   # Check the section "The node.type label" at https://github.com/MXNetEdge/benchmark-ai/blob/master/docs/autoscaler-aws.md
   # for an explanation on each type of node.
   # --feature-gates=KubeletPodResources=true required for pod-gpu-metrics
-  bai_worker_kubelet_args   = "--feature-gates='KubeletPodResources=true' --node-labels=node.type=bai-worker"
-  k8s_services_kubelet_args  = "--node-labels=node.type=k8s-services"
-  bai_services_cheap_kubelet_args = "--node-labels=node.type=bai-services-cheap"
+  bai_worker_kubelet_args           = "--feature-gates='KubeletPodResources=true' --node-labels=node.type=bai-worker"
+  k8s_services_kubelet_args         = "--node-labels=node.type=k8s-services"
+  bai_services_cheap_kubelet_args   = "--node-labels=node.type=bai-services-cheap"
   bai_services_compute_kubelet_args = "--node-labels=node.type=bai-services-compute"
   bai_services_network_kubelet_args = "--node-labels=node.type=bai-services-network"
 
@@ -50,7 +50,7 @@ locals {
   other_groups_count = 4
 
   bai_worker_group_instance_types = "${var.benchmark_workers_instance_types}"
-  worker_group_subnets = ["${module.vpc.private_subnets}"]
+  worker_group_subnets            = ["${module.vpc.private_subnets}"]
   # HACK: Terraform doesn't know how to count...
   #
   # Which means that this code: `length(module.subnets.private_subnet_ids)` would give the error:
@@ -63,55 +63,55 @@ locals {
   # Given we know that `data.aws_availability_zones` is what we're feeding the `subnets` module then we use it to get
   # the size requested.
   bai_worker_group_subnets_count = "${local.private_subnet_count}"
-  bai_worker_groups_count = "${local.bai_worker_group_subnets_count * length(local.bai_worker_group_instance_types)}"
+  bai_worker_groups_count        = "${local.bai_worker_group_subnets_count * length(local.bai_worker_group_instance_types)}"
 
-  worker_groups = "${concat(data.null_data_source.bai_worker_groups.*.outputs, local.other_worker_groups)}"
+  worker_groups       = "${concat(data.null_data_source.bai_worker_groups.*.outputs, local.other_worker_groups)}"
   worker_groups_count = "${local.other_groups_count + local.bai_worker_groups_count}"
 
   worker_group_tags = {
     k8s-services = [
       {
-        key = "k8s.io/cluster-autoscaler/node-template/label/node.type"
-        value = "k8s-services"
+        key                 = "k8s.io/cluster-autoscaler/node-template/label/node.type"
+        value               = "k8s-services"
         propagate_at_launch = true
       }
     ]
     bai-services-cheap = [
       {
-        key = "k8s.io/cluster-autoscaler/node-template/label/node.type"
-        value = "bai-services-cheap"
+        key                 = "k8s.io/cluster-autoscaler/node-template/label/node.type"
+        value               = "bai-services-cheap"
         propagate_at_launch = true
       }
     ]
     bai-services-compute = [
       {
-        key = "k8s.io/cluster-autoscaler/node-template/label/node.type"
-        value = "bai-services-compute"
+        key                 = "k8s.io/cluster-autoscaler/node-template/label/node.type"
+        value               = "bai-services-compute"
         propagate_at_launch = true
       }
     ]
     bai-services-network = [
       {
-        key = "k8s.io/cluster-autoscaler/node-template/label/node.type"
-        value = "bai-services-network"
+        key                 = "k8s.io/cluster-autoscaler/node-template/label/node.type"
+        value               = "bai-services-network"
         propagate_at_launch = true
       }
     ]
     default = [
       {
-        key = "k8s.io/cluster-autoscaler/node-template/label/node.type"
-        value = "bai-worker"
+        key                 = "k8s.io/cluster-autoscaler/node-template/label/node.type"
+        value               = "bai-worker"
         propagate_at_launch = true
       }
     ]
   }
 
   workers_group_defaults = {
-    ami_id               = "${lookup(local.ami_ids, "cpu")}"
-    key_name             = "${aws_key_pair.worker_key.key_name}"
-    autoscaling_enabled  = true
+    ami_id              = "${lookup(local.ami_ids, "cpu")}"
+    key_name            = "${aws_key_pair.worker_key.key_name}"
+    autoscaling_enabled = true
     # a work around for big files until https://github.com/MXNetEdge/benchmark-ai/issues/356 is resolved
-    root_volume_size     = "2000"
+    root_volume_size = "2000"
   }
 
   ami_ids = {
@@ -129,27 +129,27 @@ data "null_data_source" "bai_worker_groups" {
   count = "${local.bai_worker_groups_count}"
   inputs = {
     name = "bai-worker-${element(data.aws_availability_zones.available.names,
-                                 count.index % local.bai_worker_group_subnets_count)}-${element(local.bai_worker_group_instance_types,
-                                                                                                count.index / local.bai_worker_group_subnets_count)}"
+      count.index % local.bai_worker_group_subnets_count)}-${element(local.bai_worker_group_instance_types,
+    count.index / local.bai_worker_group_subnets_count)}"
     instance_type = "${element(local.bai_worker_group_instance_types,
-                               count.index / local.bai_worker_group_subnets_count)}"
+    count.index / local.bai_worker_group_subnets_count)}"
     subnets = "${element(local.worker_group_subnets,
-                         count.index % local.bai_worker_group_subnets_count)}"
-    ami_id               = "${contains(var.gpu_instance_type_prefixes,
-                                       element(split(".",
-                                                     element(local.bai_worker_group_instance_types,
-                                                             count.index / local.bai_worker_group_subnets_count))
-                                               , 0)
-                                       )
-                              ? lookup(local.ami_ids, "gpu")
-                              : lookup(local.ami_ids, "cpu")}"
-    kubelet_extra_args   = "${local.bai_worker_kubelet_args},node.gpu-type=${contains(var.gpu_instance_type_prefixes,
-                                       element(split(".",
-                                                     element(local.bai_worker_group_instance_types,
-                                                             count.index / local.bai_worker_group_subnets_count))
-                                               , 0)
-                                       )
-                              ? "NVIDIAGPU" : "NONE"}"
+    count.index % local.bai_worker_group_subnets_count)}"
+    ami_id = "${contains(var.gpu_instance_type_prefixes,
+      element(split(".",
+        element(local.bai_worker_group_instance_types,
+        count.index / local.bai_worker_group_subnets_count))
+      , 0)
+      )
+      ? lookup(local.ami_ids, "gpu")
+    : lookup(local.ami_ids, "cpu")}"
+    kubelet_extra_args = "${local.bai_worker_kubelet_args},node.gpu-type=${contains(var.gpu_instance_type_prefixes,
+      element(split(".",
+        element(local.bai_worker_group_instance_types,
+        count.index / local.bai_worker_group_subnets_count))
+      , 0)
+      )
+    ? "NVIDIAGPU" : "NONE"}"
 
     asg_min_size         = 0
     asg_desired_capacity = 0
@@ -213,8 +213,8 @@ resource "aws_key_pair" "worker_key" {
 }
 
 resource "local_file" "worker_privatekey_pem" {
-  sensitive_content  = "${tls_private_key.worker_private_key.private_key_pem}"
-  filename = "${var.data_dir}/worker_private.pem"
+  sensitive_content = "${tls_private_key.worker_private_key.private_key_pem}"
+  filename          = "${var.data_dir}/worker_private.pem"
   provisioner "local-exec" {
     # HACK while Terraform does not have a proper way to set file permissions: https://github.com/terraform-providers/terraform-provider-local/issues/19
     command = "chmod 400 ${local_file.worker_privatekey_pem.filename}"
@@ -222,9 +222,9 @@ resource "local_file" "worker_privatekey_pem" {
 }
 
 module "eks" {
-  source                               = "terraform-aws-modules/eks/aws"
-  version                              = "2.3.0"
-  cluster_name                         = "${var.cluster_name_prefix}"
+  source       = "terraform-aws-modules/eks/aws"
+  version      = "2.3.0"
+  cluster_name = "${var.cluster_name_prefix}"
   # EKS is given a fixed number of subnets.
   #
   # EKS allows to launch worker nodes in subnets that were not specified when
@@ -258,7 +258,7 @@ module "eks" {
 resource "aws_iam_role" "kube2iam-default-pod-role" {
   # The default POD role does not have any policies attached on purpose.
   # PODs that require permissions should have their own role.
-  name = "bai-default-pod-role"
+  name               = "bai-default-pod-role"
   assume_role_policy = <<-EOF
   {
     "Version": "2012-10-17",
@@ -276,8 +276,8 @@ resource "aws_iam_role" "kube2iam-default-pod-role" {
 }
 
 resource "aws_iam_role" "kube2iam-fetcher-pod-role" {
-  name = "fetcher"  # The idea is to have a very simple name here, since this role is what users will have to whitelist
-                    # in their bucket policy in order to give fetcher access to download their datasets.
+  name = "fetcher" # The idea is to have a very simple name here, since this role is what users will have to whitelist
+  # in their bucket policy in order to give fetcher access to download their datasets.
   assume_role_policy = <<-EOF
   {
     "Version": "2012-10-17",
@@ -295,9 +295,9 @@ resource "aws_iam_role" "kube2iam-fetcher-pod-role" {
 }
 resource "aws_iam_role_policy_attachment" "kube2iam-fetcher-pod-role-data-puller" {
   policy_arn = "${aws_iam_policy.data-pull-policy.arn}"
-  role = "${aws_iam_role.kube2iam-fetcher-pod-role.name}"
+  role       = "${aws_iam_role.kube2iam-fetcher-pod-role.name}"
 }
 resource "aws_iam_role_policy_attachment" "kube2iam-fetcher-pod-role-s3-read-only" {
   policy_arn = "arn:aws:iam::aws:policy/AmazonS3ReadOnlyAccess"
-  role = "${aws_iam_role.kube2iam-fetcher-pod-role.name}"
+  role       = "${aws_iam_role.kube2iam-fetcher-pod-role.name}"
 }
