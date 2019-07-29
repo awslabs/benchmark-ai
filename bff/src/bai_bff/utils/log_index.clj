8535e929ce5e2d83564aef73f4dfe5de77364568
Gavin M. Bell <gavin.max.bell@gmail.com>
2019-07-29 16:20:00 +0200

    [bff] ES fixed... (prolly)

--- bff/src/bai_bff/utils/log_index.clj
+++ bff/src/bai_bff/utils/log_index.clj
@@ -9,11 +9,11 @@
 
 (defn fetch-logs [client-id action-id]
   ;; performs a term query using a convenience function
-  (log/trace "get-job-results - client-id ["client-id"] action-id ["action-id"]")
-  (let [conn (esr/connect (str "http://"(env :es-endpoint)":80"))
-        res  (esd/search conn "logstash*" "fluentd" :query {:match {:kubernetes.labels.action-id action-id}} :sort [{(keyword "@timestamp") {:order "asc"}}])
+  (log/trace "get-fetch-logs - client-id ["client-id"] action-id ["action-id"]")
+  (let [conn (esr/connect (str "http://"(env :elasticsearch-endpoint)":80") {:content-type :json})
+        res  (esd/search conn "logstash*" "fluentd" {:query {:match {:kubernetes.labels.action-id action-id}} :sort [{(keyword "@timestamp") {:order "asc"}}]})
         n    (esrsp/total-hits res)
         hits (esrsp/hits-from res)]
     (log/debug (format "Total hits: %d" n))
-    (log/report (pp/pprint hits))
+    (log/debug (pp/pprint hits))
     (pp/pprint hits)))
