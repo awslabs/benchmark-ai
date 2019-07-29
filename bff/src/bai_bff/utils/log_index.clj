a29796e56b1c8bb4b9680f0af221a16364baf72c
Gavin M. Bell <gavin.max.bell@gmail.com>
2019-07-29 14:08:51 +0200

    [bff] ElasticSearch for results using REST interface on port 80

--- bff/src/bai_bff/utils/log_index.clj
+++ bff/src/bai_bff/utils/log_index.clj
@@ -1,18 +1,19 @@
 (ns bai-bff.utils.log-index
   (:require [environ.core :refer [env]]
             [taoensso.timbre :as log]
-            [clojurewerkz.elastisch.native          :as es]
-            [clojurewerkz.elastisch.native.document :as esd]
-            [clojurewerkz.elastisch.query           :as q]
-            [clojurewerkz.elastisch.native.response :as esrsp]
+            [clojurewerkz.elastisch.rest          :as esr]
+            [clojurewerkz.elastisch.rest.document :as esd]
+            [clojurewerkz.elastisch.query         :as q]
+            [clojurewerkz.elastisch.rest.response :as esrsp]
             [clojure.pprint :as pp]))
 
 (defn fetch-logs [client-id action-id]
   ;; performs a term query using a convenience function
   (log/trace "get-job-results - client-id ["client-id"] action-id ["action-id"]")
-  (let [conn (es/connect [[(env :es-endpoint) 443]] {"cluster.name" "logs"})
+  (let [conn (esr/connect (str "http://"(env :es-endpoint)":80"))
         res  (esd/search conn "logstash*" "fluentd" :query {:match {:kubernetes.labels.action-id action-id}} :sort [{(keyword "@timestamp") {:order "asc"}}])
         n    (esrsp/total-hits res)
         hits (esrsp/hits-from res)]
     (log/debug (format "Total hits: %d" n))
+    (log/report (pp/pprint hits))
     (pp/pprint hits)))
