33ffe0dc754f22d7fa48a7553788f9a530fef39c
Per Goncalves da Silva <perdasilva@gmail.com>
2019-08-01 15:48:12 +0200

    Fixes cron-job app

--- cron-job/src/bai_cron_job/__main__.py
+++ cron-job/src/bai_cron_job/__main__.py
@@ -1,72 +1,52 @@
-import dacite
-import json
 import logging
 import os
 import sys
-from typing import Dict
 
+import dacite
 from bai_kafka_utils.events import FetcherBenchmarkEvent
 from bai_kafka_utils.kafka_client import create_kafka_producer
+from bai_kafka_utils.logging import configure_logging
 from bai_kafka_utils.utils import generate_uuid
 
 from bai_cron_job.args import get_args
 
-logger = logging.getLogger("BAI_CRON_JOB")
-
-# Environment variable keys
-KAFKA_BOOTSTRAP_SERVERS = "KAFKA_BOOTSTRAP_SERVERS"
-PRODUCER_TOPIC = "PRODUCER_TOPIC"
-BENCHMARK_EVENT = "BENCHMARK_EVENT"
-
-
-def env_get_or_fail(key: str) -> str:
-    value = os.environ.get(key)
-    if not value:
-        raise ValueError(f"No value found for environment variable {key}")
-    return value
-
-
-def get_cron_benchmark_event() -> Dict[str, str]:
-    benchmark_event = json.loads(env_get_or_fail(BENCHMARK_EVENT))
-    required_fields = ["client_id", "message_id", "action_id"]
-    for required_field in required_fields:
-        if not benchmark_event.get(required_field):
-            raise ValueError(f"Benchmark event does not contain field {required_field}")
-
-    return benchmark_event
-
 
 def main(argv=None):
-    logger.info(f"Loading configuration {argv} {os.environ}")
-    args = get_args(argv, os.environ)
+    configure_logging(level=logging.INFO)
+    logger = logging.getLogger("BAI_CRON_JOB")
 
-    logging.info("Updating benchmark event's message and action id")
-    benchmark_event = dacite.from_dict(data_class=FetcherBenchmarkEvent, data=args.benchmark_event)
+    logger.info("Loading configuration")
+    logger.info(os.environ)
 
-    benchmark_event.message_id = generate_uuid()
-    benchmark_event.action_id = generate_uuid()
+    try:
+        args = get_args(argv, os.environ)
 
-    # Remove scheduling attribute from toml
-    info = benchmark_event.payload.toml.contents.get("info", {})
-    if "scheduling" in info:
-        info.pop("scheduling")
+        logging.info("Updating benchmark event's message and action id")
+        benchmark_event = dacite.from_dict(data_class=FetcherBenchmarkEvent, data=args.benchmark_event)
 
-    logging.info("Creating Kafka producer")
-    kafka_producer = create_kafka_producer(args.kafka_bootstrap_servers)
+        # Create new message and action ids
+        benchmark_event.message_id = generate_uuid()
+        benchmark_event.action_id = generate_uuid()
 
-    logging.info("Submitting benchmark")
-    kafka_producer.send(args.producer_topic, value=benchmark_event, key=benchmark_event["client_id"])
+        # Remove scheduling attribute from toml
+        info = benchmark_event.payload.toml.contents.get("info", {})
+        if "scheduling" in info:
+            info.pop("scheduling")
 
-    logging.debug("Closing producer")
-    kafka_producer.close()
+        logging.info("Creating Kafka producer")
+        kafka_producer = create_kafka_producer(args.kafka_bootstrap_servers)
+
+        logging.info(f"Submitting benchmark with action id {benchmark_event.action_id}")
+        kafka_producer.send(args.producer_topic, value=benchmark_event, key=benchmark_event.client_id)
+
+        logging.debug("Closing producer")
+        kafka_producer.close()
+    except Exception as err:
+        logger.error(f"Fatal error submitting benchmark job: {err}")
+        sys.exit(1)
 
     logging.info("Success")
 
 
 if __name__ == "__main__":
-    try:
-        main(sys.argv)
-    except Exception as err:
-        logger.error(f"Fatal error submitting benchmark job: {err}")
-        sys.exit(1)
-    sys.exit(0)
+    main(sys.argv)
