2d7f5fd0d2e643fbe145346b9fadc1919c321bfc
Per Goncalves da Silva <perdasilva@gmail.com>
2019-08-06 14:24:22 +0200

    Adds debug output to scheduled benchmark executor and removes unneeded consumer topic attribute

--- executor/src/executor/executor.py
+++ executor/src/executor/executor.py
@@ -17,13 +17,13 @@ logger = logging.getLogger(SERVICE_NAME)
 class ScheduledBenchmarkExecutorEventHandler(KafkaServiceCallback):
     DEFAULT_ENGINE = "default"
 
-    def __init__(self, k8s_execution_engine: K8SExecutionEngine, producer_topic: str):
+    def __init__(self, k8s_execution_engine: K8SExecutionEngine):
         self.k8s_execution_engine = k8s_execution_engine
-        self.producer_topic = producer_topic
 
     def handle_event(self, event: FetcherBenchmarkEvent, kafka_service: KafkaService):
         # Only handle scheduled benchmarks
         if ScheduledBenchmarkExecutorEventHandler.is_single_run(event):
+            logging.debug(f"Ignoring event non scheduled benchmark event: {event}")
             return
 
         try:
@@ -55,7 +55,7 @@ def create_executor(common_kafka_cfg: KafkaServiceConfig, executor_config: Execu
     kafka_service = create_executor_service(SERVICE_NAME, __version__, common_kafka_cfg, execution_engines)
 
     # Add Scheduled Benchmark Handler
-    scheduled_benchmark_handler = ScheduledBenchmarkExecutorEventHandler(k8s_engine, common_kafka_cfg.producer_topic)
+    scheduled_benchmark_handler = ScheduledBenchmarkExecutorEventHandler(k8s_engine)
     kafka_service.add_callback(scheduled_benchmark_handler, common_kafka_cfg.consumer_topic)
 
     return kafka_service
