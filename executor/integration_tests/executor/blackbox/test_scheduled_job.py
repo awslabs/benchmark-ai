9938cb4f25865a1d7224af374b3f58afedb274f8
Per Goncalves da Silva <perdasilva@gmail.com>
2019-08-15 15:38:31 +0200

    Increases timeout for test and removes cronjob deletion

--- executor/integration_tests/executor/blackbox/test_scheduled_job.py
+++ executor/integration_tests/executor/blackbox/test_scheduled_job.py
@@ -1,13 +1,10 @@
 from bai_k8s_utils.kubernetes_tests_client import KubernetesTestUtilsClient
-from bai_kafka_utils.cmd_callback import KafkaCommandCallback
 from bai_kafka_utils.events import BenchmarkEvent, FetcherBenchmarkEvent, Status
 from bai_kafka_utils.integration_tests.test_loop import (
     wait_for_response,
     CombinedFilter,
     get_is_status_filter,
     EventFilter,
-    get_cancel_event,
-    get_is_command_return_filter,
 )
 from bai_kafka_utils.kafka_service import KafkaServiceConfig
 from kafka import KafkaConsumer, KafkaProducer
@@ -55,15 +52,4 @@ def test_cron_job_exec(
         ]
     )
 
-    try:
-        wait_for_response(combined_filter, kafka_prepolled_consumer_of_produced, duration=300)
-    finally:
-        cancel_event = get_cancel_event(fetcher_benchmark_event, kafka_service_config.cmd_submit_topic, cascade=True)
-        kafka_producer_to_consume.send(
-            kafka_service_config.cmd_submit_topic, value=cancel_event, key=cancel_event.client_id
-        )
-        command_return_filter = get_is_command_return_filter(
-            cancel_event, KafkaCommandCallback.CODE_SUCCESS, kafka_service_config
-        )
-
-        wait_for_response(command_return_filter, kafka_prepolled_consumer_of_produced)
+    wait_for_response(combined_filter, kafka_prepolled_consumer_of_produced, duration=300)
