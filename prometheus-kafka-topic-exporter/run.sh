13ff18a438c718f67a7738dfbd5199aaa02ace23
Marco de Abreu <marco.g.abreu+github@gmail.com>
2019-08-19 17:03:29 -0700

    I hate bash...

--- prometheus-kafka-topic-exporter/run.sh
+++ prometheus-kafka-topic-exporter/run.sh
@@ -2,9 +2,9 @@
 set -ex
 
 # Fill in template - expects config to be mounted at /config/kafka-topic-exporter.properties
-cat /config/kafka-topic-exporter.properties.template > /tmp/kafka-topic-exporter.properties
-sed  "s/@@KAFKA_BOOTSTRAP_SERVERS@@/$KAFKA_BOOTSTRAP_SERVERS/g" /tmp/kafka-topic-exporter.properties > /tmp/kafka-topic-exporter.properties
-sed  "s/@@METRICS_CONSUMER_TOPIC@@/$METRICS_CONSUMER_TOPIC/g" /tmp/kafka-topic-exporter.properties > /tmp/kafka-topic-exporter.properties
+cp /config/kafka-topic-exporter.properties.template /tmp/kafka-topic-exporter.properties
+sed -i "s/@@KAFKA_BOOTSTRAP_SERVERS@@/$KAFKA_BOOTSTRAP_SERVERS/g" /tmp/kafka-topic-exporter.properties
+sed -i "s/@@METRICS_CONSUMER_TOPIC@@/$METRICS_CONSUMER_TOPIC/g" /tmp/kafka-topic-exporter.properties
 
 # Create Kafka Topic we're consuming
 /opt/kafka/bin/kafka-topics.sh --create \
