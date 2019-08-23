8e8474033cf762221370514bfd006618e3930ca3
stsukrov <stsukrov@amazon.com>
2019-08-22 17:02:47 -0700

    Improve logging for metrics pusher

--- metrics-pusher/src/bai_metrics_pusher/kubernetes_pod_watcher.py
+++ metrics-pusher/src/bai_metrics_pusher/kubernetes_pod_watcher.py
@@ -4,6 +4,7 @@ import time
 import signal
 from kubernetes import client, config
 from kubernetes.client import V1Pod, V1ContainerState, V1ContainerStateTerminated
+from kubernetes.client.rest import ApiException
 from threading import Thread
 
 
@@ -24,27 +25,21 @@ def start_kubernetes_pod_watcher(pod_name: str, pod_namespace: str):
 def watch_kubernetes_pod(pod_name: str, namespace: str):
     logger.info("Watch pod thread entered")
     config.load_incluster_config()
-    logger.info("Config loaded")
     v1 = client.CoreV1Api()
-    logger.info("v1 here")
 
     while True:
-        logger.info("Before get pod status")
         try:
             v1pod: V1Pod = v1.read_namespaced_pod_status(pod_name, namespace)
-        except Exception as e:
-            logger.info("Caught something %s", str(e))
+        except ApiException:
+            logger.exception("Error getting pod state")
             raise
-        finally:
-            logger.info("After get pod")
-        logger.info("pod %s read = %s", pod_name, str(v1pod))
 
         container_status_mapping = {
             container_status.name: container_status for container_status in v1pod.status.container_statuses
         }
         state: V1ContainerState = container_status_mapping["benchmark"].state
         terminated: V1ContainerStateTerminated = state.terminated
-        logger.info("Container state: %s", state)
+        logger.debug("Container state: %s", state)
 
         if terminated:
             os.kill(os.getpid(), signal.SIGTERM)
