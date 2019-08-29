162845faf85e58191152707a960c19cc1a22f653
Per Goncalves da Silva <perdasilva@gmail.com>
2019-08-29 08:01:29 +0200

    Updates to clojure naming conventions

--- bff/src/bai_bff/utils/persistence.clj
+++ bff/src/bai_bff/utils/persistence.clj
@@ -6,7 +6,7 @@
   												ResourceInUseException]))
 
 
-(def events-table "AnubisStatus")
+(def events-table "AnubisEvents")
 (def query-limit 25)
 
 (defn create-events-table []
@@ -36,17 +36,17 @@
 	(create-events-table)
 	true)
 
-(defn save-event [client_id action_id event]
- "Stores an event in the event table. The action_id is used
+(defn save-event [client-id action-id event]
+ "Stores an event in the event table. The action-id is used
   as the partition key. To guarantee order and uniqueness, a composite key
-  tstamp:message_id is used as the sort key. The action_id is set explicitly 
+  tstamp:message_id is used as the sort key. The action-id is set explicitly 
   (rather than taken directly from the event) to allow the flexibility of storing
   an event against a target action_id. For instance, we would like to log the 
   outcome of a cancel command against its target action_id."
 	(let [item {
-			:action_id action_id
+			:action_id action-id
 			:skey (str (event :tstamp) ":" (event :message_id))
-			:client_id client_id
+			:client_id client-id
 			:event event
 		}]
 			(log/trace "Saving event with client_id: " (:client_id event) " and action_id: " (:action_id event))
@@ -55,17 +55,17 @@
 
 (defn query-events-table
 	"Queries the event table for events against the supplied client_id and action_id.
-  An optional last_evaluated_key can be passed in, for pagination. Will return a dynamodb query response.
-  If it includes a last_evaluated_key, the next set of rows is retrieved."
-  [client_id action_id & {:keys [timestamp last_evaluated_key]
+  An optional last-evaluated-key can be passed in, for pagination. Will return a dynamodb query response.
+  If it includes a last-evaluated-key, the next set of rows is retrieved."
+  [client-id action-id & {:keys [timestamp last-evaluated-key]
   																								:or {timestamp 0
-  																									    last_evaluated_key nil}}]
+  																									    last-evaluated-key nil}}]
 		(let [status-query (partial ddb/query 
 				:table-name events-table 
 				:limit query-limit 
 				:key-conditions { 
 					:action_id { 
-						:attribute-value-list [action_id] 
+						:attribute-value-list [action-id] 
 						:comparison-operator "EQ"
 					}
 					:skey {
@@ -77,24 +77,24 @@
 					"#cid" "client_id"
 				}
 				:expression-attribute-values {
-					":cid" client_id
+					":cid" client-id
 				}
 				:filter-expression "#cid = :cid")]
-		 (log/trace (str "Querying events for client_id: " client_id " and action_id: " action_id))
-	 	(if (nil? last_evaluated_key)
+		 (log/trace (str "Querying events for client_id: " client-id " and action_id: " action-id))
+	 	(if (nil? last-evaluated-key)
 				(status-query)
-				(status-query :exclusive-start-key last_evaluated_key))))
+				(status-query :exclusive-start-key last-evaluated-key))))
 
 (defn get-all-events 
-	"Returns a list containing all events for a particular action_id and client_id
+	"Returns a list containing all events for a particular action-id and client-id
 	 an optional timestamp parameter to restrict the starting time of the events returned"
-	([client_id action_id] (get-all-events client_id action_id 0))
-	([client_id action_id timestamp]
-	 (log/trace (str "Getting all events for client_id: " client_id " and action_id: " action_id " from timestamp " timestamp))
-	 (loop [resp (query-events-table client_id action_id :timestamp timestamp)
+	([client-id action-id] (get-all-events client-id action-id 0))
+	([client-id action-id timestamp]
+	 (log/trace (str "Getting all events for client_id: " client-id " and action_id: " action-id " from timestamp " timestamp))
+	 (loop [resp (query-events-table client-id action-id :timestamp timestamp)
 	        events []]
 	        (println events)
 	        (if (and (not (nil? resp)) (:last-evaluated-key resp))
-	        	(recur (query-events-table client_id action_id :last_evaluated_key (:last-evaluated-key resp) :timestamp timestamp)
+	        	(recur (query-events-table client-id action-id :last_evaluated_key (:last-evaluated-key resp) :timestamp timestamp)
 	        	       (concat (map :event (:items resp)) events))
 	        	(concat (map :event (:items resp)) events)))))
