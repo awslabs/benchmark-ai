88f4f89aa547a18b6fef53d23fcf4aa985d2bd28
stsukrov <stsukrov@amazon.com>
2019-09-03 10:34:11 +0200

    Workaround pytest issue

--- blackbox-tests/tests/conftest.py
+++ blackbox-tests/tests/conftest.py
@@ -5,7 +5,8 @@ import pytest
 def bff_endpoint(pytestconfig):
     import os
 
-    return pytestconfig.getoption("--bff-endpoint", default=os.environ.get("BFF_ENDPOINT", None))
+    bff_env = os.environ.get("BFF_ENDPOINT", None)
+    return pytestconfig.getoption("--bff-endpoint") or bff_env
 
 
 def pytest_addoption(parser):
