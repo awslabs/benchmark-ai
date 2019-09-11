37398d952afb7e80fabd5ab71d15bdf1a25c8581
Jose Luis Contreras <jlcont@amazon.com>
2019-09-11 15:41:57 +0200

    [cloudwatch-exporter] Refactor to remove metrics- from metrics-cloudwatch-exporter

--- cloudwatch-exporter/setup.py
+++ cloudwatch-exporter/setup.py
@@ -3,9 +3,9 @@
 from setuptools import find_packages, setup
 
 setup(
-    name="metrics_cloudwatch_exporter",
+    name="cloudwatch_exporter",
     url="https://github.com/MXNetEdge/benchmark-ai",
     package_dir={"": "src"},
     packages=find_packages("src"),
-    entry_points={"console_scripts": ["start=metrics_cloudwatch_exporter.__main__:main"]},
+    entry_points={"console_scripts": ["start=cloudwatch_exporter.__main__:main"]},
 )
