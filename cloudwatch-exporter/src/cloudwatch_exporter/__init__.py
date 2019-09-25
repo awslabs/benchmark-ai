787c7ff955d69391b5a8ba09739ad9fb06d93156
Jose Luis Contreras <jlcont@amazon.com>
2019-09-25 17:20:10 +0200

    [cw-exporter] bump version to 1.0

--- cloudwatch-exporter/src/cloudwatch_exporter/__init__.py
+++ cloudwatch-exporter/src/cloudwatch_exporter/__init__.py
@@ -1,6 +1,6 @@
 import logging
 
-__version__ = "0.1"
+__version__ = "1.0"
 SERVICE_NAME = "cloudwatch-exporter"
 SERVICE_DESCRIPTION = "Service to listen to metrics events and post them to CloudWatch"
 
--- metrics-pusher/tests/test_args.py
+++ metrics-pusher/tests/test_args.py
@@ -6,7 +6,7 @@ from bai_metrics_pusher.args import get_input, InputValue, create_dict_of_parame
 ALL_ARGS = f"--pod-namespace pod-namespace --pod-name pod-name "
 
 
-def test_get_input_with_stdout(mocker):
+def test_get_input_with_stdout():
     expected_cfg = InputValue(backend="stdout", pod_name="pod-name", pod_namespace="pod-namespace", backend_args={})
     cfg = get_input(ALL_ARGS + " --backend stdout", environ={})
     assert cfg == expected_cfg
