fc1cba1b654bdf995c635d573a430a1f9cfceded
Per Goncalves da Silva <perdasilva@gmail.com>
2019-10-09 11:30:46 +0200

    Adds boto client error utils to kafka-utils

---
+++ kafka-utils/src/bai_sagemaker_utils/utils.py
@@ -0,0 +1,12 @@
+import re
+
+from botocore.exceptions import ClientError
+
+
+def get_client_error_message(client_error: ClientError, default: str = None):
+    return client_error.response.get("Error", {}).get("Message", default)
+
+
+def is_not_found_error(client_error: ClientError):
+    error_message = get_client_error_message(client_error, default="")
+    return re.match(r"(\w*\s*)*not found(\s*\w*)*", error_message, re.IGNORECASE) is not None
