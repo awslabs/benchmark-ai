678904a54db5e4f599de829dd9f1328df15d6b18
Ryan Steakley <ryansteakley2@gmail.com>
2020-08-24 20:07:40 -0700

    package data set to true

--- cloudwatch-exporter/setup.py
+++ cloudwatch-exporter/setup.py
@@ -1,16 +1,12 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-import os
-from os.path import join, abspath, dirname
-
 from setuptools import find_packages, setup
 
-here = abspath(dirname(__file__))
-bai_project_path = abspath(join(here, os.pardir))
 setup(
     name="cloudwatch_exporter",
     url="https://github.com/awslabs/benchmark-ai",
     package_dir={"": "src"},
     packages=find_packages("src"),
+    include_package_data=True,
     entry_points={"console_scripts": ["start=cloudwatch_exporter.__main__:main"]},
 )
