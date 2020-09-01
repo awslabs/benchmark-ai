2b512e0b9a7b1d6206d60469714c042270407d3d
Gavin M. Bell <gavin.max.bell@gmail.com>
2020-09-01 12:02:08 +0200

    Addressing flake8 linting issues with python files with headers added

--- metrics-extractor/src/anubis_metrics_extractor/__main__.py
+++ metrics-extractor/src/anubis_metrics_extractor/__main__.py
@@ -1,3 +1,5 @@
+#!/usr/bin/env python
+
 #  Copyright 2019 Amazon.com, Inc. or its affiliates. All Rights Reserved.
 #
 #  Licensed under the Apache License, Version 2.0 (the "License").
@@ -10,7 +12,6 @@
 #  on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either
 #  express or implied. See the License for the specific language governing
 #  permissions and limitations under the License.
-#!/usr/bin/env python
 
 # The default pattern which would in most cases be just accuracy is
 # {"name":"accuracy", "pattern":"YWNjdXJhY3k9KFstK10/XGQqXC5cZCt8XGQrKQ=="}
--- metrics-pusher/src/bai_metrics_pusher/__main__.py
+++ metrics-pusher/src/bai_metrics_pusher/__main__.py
@@ -1,3 +1,5 @@
+#!/usr/bin/env python
+
 #  Copyright 2019 Amazon.com, Inc. or its affiliates. All Rights Reserved.
 #
 #  Licensed under the Apache License, Version 2.0 (the "License").
@@ -10,8 +12,6 @@
 #  on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either
 #  express or implied. See the License for the specific language governing
 #  permissions and limitations under the License.
-#!/usr/bin/env python
-
 
 def main(argv=None):
     # We import only inside the `main()` method to ensure we configure everything so that nothing from the global
